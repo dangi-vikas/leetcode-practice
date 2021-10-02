b3b417b87e5c7c56474e18693db63e9bdf67a2bc
Vikas Dangi <mvikasdangi@gmail.com>
2021-10-02 23:19:22 +0530

    Two Sum

---
+++ problems/1-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int len = nums.size();
+        vector<int> v;
+        
+        for(int i=0; i<len-1; i++){
+            for(int j=i+1; j<len; j++){
+                if(nums[i] + nums[j] == target){                //when the two numbers' sum is equal to target
+                    v.push_back(i);
+                    v.push_back(j);                             //pushing them in the vector
+                }
+            }
+        }
+        
+        return v;
+    }
+};
