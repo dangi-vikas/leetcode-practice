ebae4571fb4b558a741a49d8becaf39c496effea
Vikas Dangi <mvikasdangi@gmail.com>
2021-10-13 23:12:14 +0530

    Rotate Image

---
+++ problems/48-rotate-image.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        
+        //finding the transpose of the matrix
+        for(int i=0; i<n; i++)
+            for(int j=0; j<i; j++) swap(matrix[i][j], matrix[j][i]);
+
+        //now reversing the rows will give the desired answer
+        for(int i=0; i<n; i++) reverse(matrix[i].begin(), matrix[i].end());
+    }
+};
