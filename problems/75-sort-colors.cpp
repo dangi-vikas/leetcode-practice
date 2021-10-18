59e8151a454e64e556807259d0146ece3d9bdde1
Vikas Dangi <mvikasdangi@gmail.com>
2021-10-18 17:25:22 +0530

    Sort Colors

---
+++ problems/75-sort-colors.cpp
@@ -0,0 +1,26 @@
+//Dutch National Flag Algorithm
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0, high=nums.size()-1; 
+        int mid = 0;
+        
+        while(high>=mid){
+            switch(nums[mid]){
+
+                //if the element is 0
+                case 0: swap(nums[low++], nums[mid++]);
+                        break;
+
+                //if the element is 1   
+                case 1: mid++;
+                        break;
+                
+                //if the element is 2
+                case 2: swap(nums[high--], nums[mid]);
+                        break;
+            }
+        }
+    }
+};
