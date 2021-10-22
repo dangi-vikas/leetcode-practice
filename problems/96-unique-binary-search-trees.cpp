9a8d35c035ba0a25ce6db20adfdde939dfce4b2a
Vikas Dangi <mvikasdangi@gmail.com>
2021-10-22 20:52:08 +0530

    Unique Binary Search Trees

---
+++ problems/96-unique-binary-search-trees.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        //catalan number formula
+        for(int i=2; i<=n; i++){
+            int l=0, r=i-1;
+            
+            while(l<=i-1){
+                dp[i] += dp[l]*dp[r];
+
+                l++; r--;
+            }
+        }
+        
+        return dp[n];
+    }
+};
