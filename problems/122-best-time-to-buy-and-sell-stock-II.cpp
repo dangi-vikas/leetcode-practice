3afa412cca6b38a556e591e4d367436181c60ce3
Vikas Dangi <mvikasdangi@gmail.com>
2021-10-29 17:05:31 +0530

    Best Time to Buy and Sell Stock II

---
+++ problems/122-best-time-to-buy-and-sell-stock-II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        for(int i=1; i<prices.size(); i++){
+            
+            //finding the maximum profit by peak valley approach
+            if(prices[i]>prices[i-1])
+                profit += prices[i] - prices[i-1];
+        }
+        
+        return profit;
+    }
+};
