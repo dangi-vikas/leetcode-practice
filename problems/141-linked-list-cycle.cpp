05edb438b293c3e5f43554f9e9f98b33ffe7107d
Vikas Dangi <mvikasdangi@gmail.com>
2021-11-02 18:38:30 +0530

    Linked List Cycle

---
+++ problems/141-linked-list-cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) return false;
+        
+        ListNode* fast=head, *slow=head;
+        
+        while(fast->next && fast->next->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            //if there exist a cycle at some point fast and slow will concide
+            if(slow == fast) return true;
+        }
+        
+        return false;
+    }
+};
