007336e9e413fc5faeecc74f01f42aa7bce6ae44
Vikas Dangi <mvikasdangi@gmail.com>
2021-11-12 17:03:08 +0530

    Number of 1 Bits

---
+++ problems/191-number-of-1-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        
+        for(int i=0; i<32; i++){
+
+            //if the bit is one increment the count
+            if(n&1) count++;
+
+            //shifting left
+            n>>=1;
+        }
+        
+        return count;
+    }
+};
