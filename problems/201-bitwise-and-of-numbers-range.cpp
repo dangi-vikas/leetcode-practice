4337d500b3a59f3d6d59feecf6a546c41b869407
Vikas Dangi <mvikasdangi@gmail.com>
2021-11-13 20:54:08 +0530

    Bitwise AND of Numbers Range

---
+++ problems/201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+
+        //checking the common bits
+        while(left != right){
+            left >>= 1;
+            right >>= 1;
+            count++;
+        }
+        
+        //returning the bits after adding count zeroes at end
+        return left<<count;
+    }
+};
