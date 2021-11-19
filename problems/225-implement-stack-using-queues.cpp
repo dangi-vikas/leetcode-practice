5f5190c33a5904742c04099f94adaf669dbf1a13
dangi-vikas <mvikasdangi@gmail.com>
2021-11-19 20:27:56 +0530

    Implement Stack using Queues

---
+++ problems/225-implement-stack-using-queues.cpp
@@ -0,0 +1,43 @@
+class MyStack {
+public:
+    queue<int> q;
+    queue<int> qTemp;
+    
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        qTemp.push(x);
+        
+        while(!q.empty()){
+            qTemp.push(q.front()); q.pop();
+        }
+        
+        queue<int> temp = q;
+        q = qTemp;
+        qTemp = temp;
+    }
+    
+    int pop() {
+        int temp = q.front(); q.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
