6b8db689c8a710d11a5c7b73436624064b3dcaf8
dangi-vikas <mvikasdangi@gmail.com>
2021-12-12 18:34:25 +0530

    Intersection of Two Arrays II

---
+++ problems/350-intersection-of-two-arrays-II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> count;
+        vector<int> ans;
+        
+        for(auto i : nums1) count[i]++;
+        
+        for(auto i : nums2)
+            if(count[i]){
+                ans.push_back(i); 
+                count[i]--;
+            }
+        
+        return ans;
+    }
+};
