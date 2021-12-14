16ba2487ceab075785fd4443b06a735e878de2da
dangi-vikas <mvikasdangi@gmail.com>
2021-12-14 19:06:02 +0530

    Sum of Two Integers

---
+++ problems/371-sum-of-two-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b){
+            int temp = (unsigned) (a & b) << 1;
+            a ^= b;
+            b = temp;
+        }
+        
+        return a;
+    }
+};
