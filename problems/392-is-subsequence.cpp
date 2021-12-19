afaf576ec6a5d24c2efc4dc554d07553dce384f1
dangi-vikas <mvikasdangi@gmail.com>
2021-12-19 13:24:17 +0530

    Is Subsequence

---
+++ problems/392-is-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0, j=0;
+        int n=t.length(), m=s.length();
+        
+        if(m == 0) return true;
+        
+        while(j < n){
+            if(s[i] == t[j]){
+                i++;
+                if(i == m) return true;
+            }
+            
+            j++;
+        }
+        
+        return false;
+    }
+};
