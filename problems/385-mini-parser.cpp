4a058d5c0264f2751b944adb001e23315e6142f6
dangi-vikas <mvikasdangi@gmail.com>
2021-12-24 21:55:41 +0530

    Mini Parser

---
+++ problems/385-mini-parser.cpp
@@ -0,0 +1,60 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+        int i = 0;
+        return deserialize(s, i); 
+    }
+    
+    NestedInteger deserialize(string& s, int& i) {
+       NestedInteger ans;
+        
+       if(s[i] == '[') {
+           while(s[i] != ']') {
+               ++i;
+               if(s[i] == ']') break;
+               ans.add(deserialize(s,i));
+           }
+           ++i;
+       }
+        
+       else {
+           int j = i;
+           
+           while(j < s.size() && s[j] != ',' && s[j] != ']') ++j;
+           ans.setInteger(stoi(s.substr(i, j-i)));
+           i = j;
+       }
+        
+       return ans;
+    }
+};
