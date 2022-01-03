dcb40d228073bf55c97454dcebb726c858b4273a
dangi-vikas <mvikasdangi@gmail.com>
2022-01-03 20:41:57 +0530

    Maximum XOR of Two Numbers in an Array

---
+++ problems/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,79 @@
+class Solution {
+    struct Node {
+    private:
+        Node *links[2];
+        
+    public:
+//         Node(){
+//             links[0] = NULL;
+//             links[1] =  NULL;
+//         }
+        
+        bool containsKey(int bit){
+            return (links[bit] != NULL);
+        }
+        
+        Node* get(int bit){
+            return links[bit]; 
+        }
+        
+        void put(int bit, Node* node){
+            links[bit] = node;
+        }
+    };
+    
+    class Trie{
+        Node* root;
+        
+    public:
+        Trie(){
+            root = new Node();
+        }
+        
+        void insert(int num){
+            Node* node = root;
+            
+            for(int i=31; i>=0; i--){
+                int bit = (num >> i) & 1;
+                
+                if(!node->containsKey(bit))
+                    node->put(bit, new Node());
+                
+                node = node->get(bit);
+            }
+        }
+        
+        int getMax(int num){
+            Node* node = root;
+            int maxNum = 0;
+
+            for(int i=31; i>=0; i--){
+                int bit = (num >> i) & 1;
+
+                if(node->containsKey(1-bit)){
+                    maxNum = maxNum | (1<<i);
+                    node = node->get(1-bit); 
+                }
+
+                else node = node->get(bit);   
+            }
+
+            return maxNum;
+        } 
+    };
+    
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie;
+        
+        for(auto& it : nums) trie.insert(it);
+        
+        int ans = 0;
+        
+        for(auto& it : nums)
+            ans = max(ans, trie.getMax(it));
+        
+        return ans;
+    }
+    
+};
