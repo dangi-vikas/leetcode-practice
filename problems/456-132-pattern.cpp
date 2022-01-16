1d933df211a5a617abab1e4befdef42d40d9c1ed
dangi-vikas <mvikasdangi@gmail.com>
2022-01-16 23:52:28 +0530

    132 Pattern

---
+++ problems/456-132-pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> st;
+        int prev = INT_MIN;
+        
+        for(int i=n-1; i>=0; i--){
+            if(nums[i] < prev) return true;
+            
+            while(st.size() && st.top() < nums[i]){
+                prev = max(prev, st.top());
+                st.pop();
+            }
+                
+            st.push(nums[i]);
+        }
+        
+        return false;
+    }
+};
