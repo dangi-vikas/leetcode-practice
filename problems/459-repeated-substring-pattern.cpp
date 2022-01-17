d066978d9376981ebb5150d0759f28921883d64a
dangi-vikas <mvikasdangi@gmail.com>
2022-01-17 21:03:01 +0530

    Repeated Substring Pattern

---
+++ problems/459-repeated-substring-pattern.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        vector<int> lps(n);
+        
+        KMP(s, lps);
+        
+        return lps[n-1] != 0 && n % (n - lps[n-1]) == 0;
+    }
+    
+    void KMP(string &s, vector<int>&lps) {
+        lps[0] = 0;
+        int i = 0, j = 1;
+        
+        while(j < s.length()) {
+            if(s[i] == s[j]) {
+                lps[j] = (i) + 1;
+                i++; j++;
+            }
+            
+            else {
+                if(i == 0) {
+                    lps[j] = 0;
+                    j++;
+                }
+                
+                else i = lps[i-1];
+            }
+        }
+    }
+};
