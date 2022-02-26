a3138731fcb330d77dbec89d67a33a8e2d9b61a0
dangi-vikas <mvikasdangi@gmail.com>
2022-02-26 22:53:52 +0530

    N-ary Tree Postorder Traversal

---
+++ problems/590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if(!root) return {};
+        
+        vector<int> ans; 
+        
+        for(int i = 0; i<root->children.size(); i++){
+            vector<int> temp = postorder(root->children[i]);
+            for(int j=0; j<temp.size(); j++) 
+                ans.push_back(temp[j]);
+        }
+        
+        ans.push_back(root->val);
+        
+        return ans;
+    }
+};
