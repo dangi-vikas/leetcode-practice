d1f7ffe03399f5873b72ef53783cd666083a8ab1
dangi-vikas <mvikasdangi@gmail.com>
2022-03-06 23:35:42 +0530

    Task Scheduler

--- problems/621-task-scheduler.cpp
+++ problems/621-task-scheduler.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, char> counts;
-        for(char t : tasks) counts[t]++;
+    int leastInterval(vector<char>& tasks, int k) {
+        unordered_map<char, int> freq;
+        for (auto &task : tasks) freq[task]++;
         
-        priority_queue<int> pq;
-        for(auto c: counts) pq.push(c.second);
+        vector<pair<int, char>> frequencies;
+        int n = freq.size();
+        frequencies.reserve(n);
+         
+        for (auto &it : freq) 
+          frequencies.push_back({it.second, it.first});
         
-        int ans = 0;
-        while(!pq.empty()) {
-            int time = 0;
-            vector<int> temp;
-            
-            for(int i=0; i<n+1; i++) {
-                if(!pq.empty()) {
-                    temp.push_back(pq.top() - 1);
-                    pq.pop();
-                    time++;
-                }
-            }
-            
-            for(auto t : temp)
-                if(t) pq.push(t);
-            
-            ans += pq.empty() ? time : n+1;  
-        }
+        sort(frequencies.begin(), frequencies.end(), [](auto &p1, auto &p2) { return p1.first > p2.first; });
         
-        return ans;
+        int maxFreq = frequencies[0].first;
+        int idleTime = (maxFreq - 1) * k; 
+        
+        for (int i = 1; i < n; ++i) 
+          idleTime -= min(maxFreq - 1, frequencies[i].first);
+        
+        return max(0, idleTime) + tasks.size();
     }
 };
