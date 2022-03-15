8607367233690ac42215876451064b87cd38c030
dangi-vikas <mvikasdangi@gmail.com>
2022-03-15 20:58:17 +0530

    Maximum Length of Pair Chain

---
+++ problems/646-maximum-length-of-pair-chain.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    static bool comparator(vector<int>& a, vector<int>& b) {
+        return a[1] < b[1]; 
+    }
+    
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), comparator);
+        
+        int ans = 1, prev = pairs[0][1];
+        
+        for(int i=1; i<pairs.size(); i++) {
+            if(prev < pairs[i][0]) {
+                prev = pairs[i][1];
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+};
