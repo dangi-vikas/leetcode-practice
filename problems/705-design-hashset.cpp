bbaa2b4af7367832e19735e3dab9afa9630052ed
dangi-vikas <mvikasdangi@gmail.com>
2022-04-05 22:42:04 +0530

    Design HashSet

---
+++ problems/705-design-hashset.cpp
@@ -0,0 +1,28 @@
+class MyHashSet {
+    vector<int> set;
+public:
+    MyHashSet() {
+        int s = 1e6 + 1;
+        set.resize(s);
+    }
+    
+    void add(int key) {
+        set[key] = 1;
+    }
+    
+    void remove(int key) {
+        set[key] = 0;
+    }
+    
+    bool contains(int key) {
+        return set[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
