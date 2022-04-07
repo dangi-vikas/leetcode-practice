6d6cde4b31015cfd951e0fddc5e423f723aa68c8
dangi-vikas <mvikasdangi@gmail.com>
2022-04-07 21:44:19 +0530

    Subarray Product Less Than K

---
+++ problems/713-subarray-product-less-than-k.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k <= 1) return 0;
+        
+        int n = nums.size();
+        int count = 0, start = 0, end = 0;
+        long long currProd = 1;
+        
+        while(end < n) {
+            currProd *= nums[end];
+            
+            while(start <= end && currProd >= k) {
+                currProd /= nums[start];
+                start++;
+            }
+            
+            count += end - start + 1;
+            end++;
+        }
+        
+        return count;
+    }
+};
