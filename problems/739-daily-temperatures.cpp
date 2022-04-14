0bd1cd1d184cfea6131ed27282f4bb65c87e738f
dangi-vikas <mvikasdangi@gmail.com>
2022-04-14 21:54:40 +0530

    Daily Temperatures

---
+++ problems/739-daily-temperatures.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<int> st;
+        vector<int> ans(n, 0);
+        
+        for(int i=0; i<n; i++) {
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            
+            st.push(i);
+        }
+        
+        return ans;
+    }
+};
