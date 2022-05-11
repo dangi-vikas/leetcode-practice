334ad0af2164e422d8a1c0a2ed6261457d97f1a3
dangi-vikas <mvikasdangi@gmail.com>
2022-05-11 23:35:38 +0530

    Most Profit Assigning Work

---
+++ problems/826-most-profit-assigning-work.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        map<int, int> maxes;
+        
+        for(int i = 0;i<difficulty.size();i++) {
+            if(maxes.find(difficulty[i]) == maxes.end()) 
+                maxes[difficulty[i]] = profit[i];
+    
+            else maxes[difficulty[i]] = max(maxes[difficulty[i]],profit[i]);
+        }
+        
+        vector<int> uniqueDifficulties;
+        
+        for(auto& i : maxes)
+            uniqueDifficulties.push_back(i.first);
+        
+        vector<int> ordering(uniqueDifficulties.size()+1, INT_MIN); 
+        
+        for(int i = 1; i<uniqueDifficulties.size() + 1; i++)
+            ordering[i] = max(ordering[i-1],maxes[uniqueDifficulties[i-1]]);
+        
+        int tot = 0;
+        
+        for(int i = 0; i<worker.size(); i++) {    
+            int index = upper_bound(uniqueDifficulties.begin(),uniqueDifficulties.end(),worker[i])-uniqueDifficulties.begin()-1;
+            if(index == -1) continue; 
+            tot += ordering[index+1];
+        }
+        
+        return tot;
+    } 
+};
