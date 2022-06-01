f951e17ccd70ff49b25d9fa2153faa149b724225
dangi-vikas <mvikasdangi@gmail.com>
2022-06-01 21:46:05 +0530

    Car Fleet

---
+++ problems/853-car-fleet.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        
+        if (n == 1) return 1;
+        
+        vector<pair<int, double>> startPosAndTravelTime(n);
+        
+        for (int i = 0; i <n;  i++) 
+            startPosAndTravelTime[i] = {position[i], ((double) target - position[i]) / speed[i]};
+        
+        sort(startPosAndTravelTime.begin(), startPosAndTravelTime.end());
+        
+        double currTime = startPosAndTravelTime[n-1].second;
+        int ans = 1;
+        
+        for (int i=n-2; i>=0; i--) {
+            if (startPosAndTravelTime[i].second <= currTime)
+                continue;
+            
+            ans++;
+            currTime = startPosAndTravelTime[i].second;
+        }
+        
+        return ans;
+    }
+};
