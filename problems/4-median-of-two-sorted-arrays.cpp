18dcde79a5632c01118a8ebaf9f4707b6ee78e15
dangi-vikas <mvikasdangi@gmail.com>
2022-06-10 18:52:41 +0530

    Median of Two Sorted Arrays

---
+++ problems/4-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        double median = 0;
+        
+        for(int num2 : nums2) nums1.push_back(num2);
+        
+        sort(nums1.begin(), nums1.end());
+        
+        int n = nums1.size();
+        int h = n >> 1;
+        
+        if(n%2 == 0) median = (double) (nums1[h] + nums1[h-1]) / 2;
+        else median = nums1[h];
+        
+        return median;
+    }
+};
