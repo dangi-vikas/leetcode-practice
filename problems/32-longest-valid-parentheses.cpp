7ae8936f9bfcea122fa2e65842559bf93273788a
dangi-vikas <mvikasdangi@gmail.com>
2022-06-20 21:57:49 +0530

    Longest Valid Parentheses

---
+++ problems/32-longest-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int mx = 0;
+        vector<int> dp(s.length());
+        
+        for(int i=1; i<s.length(); i++) {
+            if(s[i]==')'){
+                if(s[i-1]=='(') 
+                    dp[i] = (i>=2 ? dp[i-2] : 0) + 2;
+                else if(i-dp[i-1]-1 >=0 && s[i-dp[i-1]-1] == '(')
+                    dp[i] = dp[i-1] + ((i-dp[i-1])>=2 ? dp[i-dp[i-1] - 2] : 0) + 2;
+            }   
+            
+            mx = max(mx, dp[i]);
+        }
+        
+        return mx;
+    }
+};
