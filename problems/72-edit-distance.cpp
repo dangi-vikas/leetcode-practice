327543bdc21b971d70fff960ed82485d447b5570
dangi-vikas <mvikasdangi@gmail.com>
2022-07-03 19:51:53 +0530

    Edit Distance

---
+++ problems/72-edit-distance.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+        
+        for(int i=0; i<m+1; i++) prev[i] = i;
+                                  
+        for(int i=1; i<n+1; i++) {
+            curr[0] = i;
+            
+            for(int j=1; j<m+1; j++){
+                if(word1[i-1] == word2[j-1])
+                    curr[j] = prev[j-1];
+                
+                else curr[j] = 1 + min(prev[j] , min(curr[j-1], prev[j-1]));
+            } 
+            
+            prev = curr;
+        }
+        
+        return prev[m];
+    }
+};
