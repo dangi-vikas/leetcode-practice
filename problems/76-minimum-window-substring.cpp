bd53e6c072eecce907d63e5647360cf34a530a8f
dangi-vikas <mvikasdangi@gmail.com>
2022-07-04 22:29:24 +0530

    Minimum Window Substring

---
+++ problems/76-minimum-window-substring.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> freq;
+        int count = 0, minCount = s.size();
+        int winStart = -1, winEnd = -1;
+        int start = 0, end = 0;
+        
+        for(int ch : t) freq[ch]++;
+        
+        for(; end<s.size(); end++) {
+            if(freq.find(s[end]) != freq.end()) {
+                freq[s[end]]--;
+                
+                if(freq[s[end]] == 0) count++; 
+            }
+            
+            while(count == freq.size()) {
+                if(end-start+1 <= minCount) {
+                    minCount = end-start+1;
+                    winEnd = end;
+                    winStart = start;
+                }
+                
+                if(freq.find(s[start]) != freq.end()) {
+                    if(freq[s[start]] == 0) count--;
+                    freq[s[start]]++;
+                }
+                
+                start++;
+            }
+        }
+        
+        string ans = ""; 
+        
+        if(winStart != -1)
+            for(int i=winStart; i<=winEnd; i++)
+                ans += s[i];
+            
+        return ans;
+    }
+};
