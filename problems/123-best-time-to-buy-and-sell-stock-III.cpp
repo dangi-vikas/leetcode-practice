25d30924767219c66ddcca006d2fb2cb16c73940
dangi-vikas <mvikasdangi@gmail.com>
2022-07-17 18:08:04 +0530

    Best Time to Buy and Sell Stock III

--- problems/123-best-time-to-buy-and-sell-stock-III.cpp
+++ problems/123-best-time-to-buy-and-sell-stock-III.cpp
@@ -1,3 +1,5 @@
+//Solution 1
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -14,5 +16,34 @@ public:
         }
         
         return next[0];
+    }
+};
+
+
+//Solution 2
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> next(2, vector<int>(3,0));
+      
+        for(int idx=n-1; idx>=0; idx--){
+            for(int buy=0; buy<2; buy++){
+                for(int cap=1; cap<3; cap++){
+                    int profit = 0;
+
+                    if(buy == 1)
+                       profit= max(-prices[idx] + next[0][cap], next[1][cap]);
+                    
+                    if(buy==0)
+                        profit= max(prices[idx] + next[1][cap-1], next[0][cap]);
+                    
+                     next[buy][cap]= profit;
+                }
+            }
+        }
+        
+        return next[1][2];
     }
 };
