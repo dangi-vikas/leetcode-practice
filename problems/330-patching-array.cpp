9bb776594dde293f027aa29559baa564edc4730f
dangi-vikas <mvikasdangi@gmail.com>
2022-07-26 20:12:05 +0530

    Patching Array

---
+++ problems/330-patching-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        int count = 0, i = 0;
+        long reach = 0;
+        
+        while(reach < n) {
+            if(i < nums.size() && nums[i] <= (reach+1)) {
+                reach += nums[i]; 
+                i++;
+            }
+            
+            else {
+                reach += reach + 1;
+                count ++;
+            }
+        }
+        
+        return count;
+    }
+};
