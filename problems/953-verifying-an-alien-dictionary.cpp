28fea6c63d684a262c4bc15eafe2a913029205a5
dangi-vikas <mvikasdangi@gmail.com>
2022-08-09 23:26:12 +0530

    Verifying an Alien Dictionary

---
+++ problems/953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        int order1, order2;
+         
+        for (int i=1; i<words.size(); i++) { 
+            for (int j=0; j<words[i-1].size() && j<words[i].size(); j++) { 
+                order1 = order.find(words[i-1][j]); 
+                order2 = order.find(words[i][j]); 
+                
+                if (order1 < order2) j = words[i-1].size();
+                else if (order1 > order2) return false; 
+                else if (j == words[i].size() - 1 && words[i].size() < words[i-1].size()) 
+                    return false; 
+            }
+        }
+        
+        return true; 
+    }
+};
