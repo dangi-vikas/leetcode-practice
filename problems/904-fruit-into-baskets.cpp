1af52f8dee8fae194ced394b079bc812aae460a4
dangi-vikas <mvikasdangi@gmail.com>
2022-08-29 21:27:55 +0530

    Fruit Into Baskets

---
+++ problems/904-fruit-into-baskets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int start = 0, end = 0, maxFruits = 0;
+        unordered_map<int, int> lastSeen;
+        
+        while(end < fruits.size()) {
+            lastSeen[fruits[end]]++;
+            
+            if(lastSeen.size() >= 3) {
+                lastSeen[fruits[start]]--;
+                
+                if(lastSeen[fruits[start]] == 0) 
+                    lastSeen.erase(fruits[start]);
+                 
+                start++;
+            }
+            
+            end++;
+        }
+        
+        return end - start;
+    }
+};
