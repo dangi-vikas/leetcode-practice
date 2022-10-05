7d1da694cea98edc5b017cf7f90cff89acca548d
dangi-vikas <mvikasdangi@gmail.com>
2022-10-05 20:45:14 +0530

    Minimum Area Rectangle

--- problems/939-minimum-area-rectangle.cpp
+++ problems/939-minimum-area-rectangle.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minAreaRect(vector<vector<int>>& points) {
         int ans = INT_MAX;
-        unordered_map<int, unordered_map<int>> m;
+        unordered_map<int, unordered_set<int>> m;
         
         for(vector<int> p : points) m[p[0]].insert(p[1]);
         
@@ -12,11 +12,11 @@ public:
                 int y1 = points[i][1];  
                 int x2 = points[j][0];  
                 int y2 = points[j][1];
+           
+                if((x1 != x2) && (y2 != y1))
+                    if(m[x1].find(y2) != m[x1].end() && m[x2].find(y1) != m[x2].end())
+                        ans = min(ans, abs(x1-x2) * abs(y1-y2));
             }
-            
-            if((x != x2) && (y2 != y1))
-                if(m[x1].find(y2) != m[x1].end() && m[x2].find(y1) != m[x2].end())
-                    ans = min(ans, abs(x1-x2) * abs(y1-y2));
         }
         
         return ans == INT_MAX ? 0 : ans;
