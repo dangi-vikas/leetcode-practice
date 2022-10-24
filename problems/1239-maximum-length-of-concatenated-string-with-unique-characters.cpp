b32fac6b3826dfd83b5c67e6bdd5f30a700c69ca
dangi-vikas <mvikasdangi@gmail.com>
2022-10-24 15:43:07 +0530

    MaxLength of Concatenated String with Unique Chars

---
+++ problems/1239-maximum-length-of-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    int ans = 0;
+    
+public:
+    int maxLength(vector<string>& arr) {
+        maxLengthRec(arr, 0, "", arr.size());
+        
+        return ans;
+    }
+    
+    void maxLengthRec(vector<string>& arr, int index, string container, int n) {
+        if(!isUnique(container)) return;
+        
+        if(index == n) {
+            int size = container.size();
+            ans = max(ans, size);
+            return;
+        }
+        
+        maxLengthRec(arr, index+1, container+arr[index], n);
+        maxLengthRec(arr, index+1, container, n);
+    }
+    
+    bool isUnique(string& s) {
+        unordered_set<char> sSet;
+        
+        for(char ch : s) {
+            if(sSet.find(ch) != sSet.end())
+                return false;
+            
+            sSet.insert(ch);
+        }
+        
+        return true;
+    }
+};
