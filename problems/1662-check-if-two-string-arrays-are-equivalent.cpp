2e8f9f59b694350df65193c7612b682acbf05308
dangi-vikas <mvikasdangi@gmail.com>
2022-10-25 14:56:25 +0530

    Check If Two String Arrays are Equivalent

---
+++ problems/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string w1, w2;
+        
+        for(string str : word1) w1 += str;
+        for(string str : word2) w2 += str;
+        
+        return w1 == w2;
+    }
+};
