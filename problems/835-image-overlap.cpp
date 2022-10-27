cb00e8b992f12d461a5483b88bb631e778cd8d1b
dangi-vikas <mvikasdangi@gmail.com>
2022-10-27 18:20:36 +0530

    Image Overlap

--- problems/835-image-overlap.cpp
+++ problems/835-image-overlap.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         int n = img1.size();
-        vector<pair<int, int>> vecA;
-        vector<pair<int, int>> vecB;
+        vector<pair<int, int>> ones1, ones2;
+        map<pair<int, int>, int> onesPairs;
         int ans = 0;
-        map<pair<int, int>, int> mp;
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(img1[i][j] == 1) vecA.push_back({i,j});
-                if(img2[i][j] == 1) vecB.push_back({i,j});
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(img1[i][j] == 1) ones1.push_back({i, j});
+                if(img2[i][j] == 1) ones2.push_back({i, j});
             }
         }
         
-        
-        for(auto [i1, j1] : vecA){
-            for(auto [i2,j2] : vecB){
-                mp[{i1-i2, j1-j2}]++;
-                ans = max(ans, mp[{i1-i2, j1-j2}]);
+        for(auto [i1, j1] : ones1) {
+            for(auto [i2, j2] : ones2) {
+                onesPairs[{i1-i2, j1-j2}]++;
+                ans = max(ans, onesPairs[{i1-i2, j1-j2}]);
             }
         }
         
