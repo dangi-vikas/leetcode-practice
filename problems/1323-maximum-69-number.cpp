6da12d510f3391316c63b0aa495fed6098dae163
dangi-vikas <mvikasdangi@gmail.com>
2022-11-07 14:45:06 +0530

    Maximum 69 Number

---
+++ problems/1323-maximum-69-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string n = to_string(num);
+        cout<<n;
+        
+        for(int i=0; i<n.length(); i++) {
+            if(n[i] == '6') {
+                n[i] = '9';
+                break;
+            }
+        }
+        
+        return stoi(n);
+    }
+};
