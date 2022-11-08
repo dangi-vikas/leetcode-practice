e29a0b8264a3f085204737ee4fb9a44b3da5f148
dangi-vikas <mvikasdangi@gmail.com>
2022-11-08 18:41:00 +0530

    Make The String Great

---
+++ problems/1544-make-the-string-great.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        int i = 0;
+        
+        while (i < s.length()) {
+            if(abs(s[i+1] - s[i]) == 32) {
+                s.erase(i, 2);
+                i = 0;
+            }  
+            
+            else i++;
+        }
+            
+        return s;
+    }       
+};
