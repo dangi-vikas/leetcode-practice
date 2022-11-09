5b11c0fc799d26967b82585a492979d042041607
dangi-vikas <mvikasdangi@gmail.com>
2022-11-09 19:53:23 +0530

    Online Stock Span

--- problems/901-online-stock-span.cpp
+++ problems/901-online-stock-span.cpp
@@ -1,26 +1,22 @@
 class StockSpanner {
-    stack<pair<int, int>> st;
-    int index;
+    stack<pair<int, int>> stocks;
     
 public:
     StockSpanner() {
-        index = -1;
+        
     }
     
     int next(int price) {
-        index++;
+        int ans = 1;
         
-        while(!st.empty() && st.top().second <= price) st.pop();
-        
-        if(st.empty()) {
-            st.push({index, price});
-            return index+1;
+        while(!stocks.empty() && price >= stocks.top().first) {
+            ans += stocks.top().second;
+            stocks.pop();
         }
         
-        int result = st.top().first;
-        st.push({index, price});
+        stocks.push({price, ans});
         
-        return index - result;
+        return ans;
     }
 };
 
