31bcf953cd6fb1060905bca050b49b0bbe52ef72
dangi-vikas <mvikasdangi@gmail.com>
2022-11-10 11:56:59 +0530

    Remove All Adjacent Duplicates In String

--- problems/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ problems/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string str;
+        string st = "";
+        int i = 0;
         
-        for(char ch : s) {
-            if(!str.empty() && str.back() == ch)
-                str.pop_back();
+        while(i < s.length()) {
+            if(!st.empty() && s[i] == st[st.size()-1]) 
+                st.pop_back();
+                
+            else st.push_back(s[i]);
             
-            else str.push_back(ch);
+            i++;
         }
         
-        return str;
+        return st;
     }
 };
+
+
+
