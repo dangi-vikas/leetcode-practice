319a3ce9c1ef4da0263ffdc71836d3e70f23613c
dangi-vikas <mvikasdangi@gmail.com>
2022-11-11 12:12:01 +0530

    Remove Duplicates Sorted Array

--- problems/26-remove-duplicates-sorted-array.cpp
+++ problems/26-remove-duplicates-sorted-array.cpp
@@ -5,18 +5,20 @@ public:
         //if the nums array is empty
         if(nums.size()==0) return 0;
         
-        int i=0;
+        int i = 1, j = 1;
         
-        for(int j=1; j<nums.size(); j++){
+        while(j < nums.size()){
 
             //if j is a different number swapping overriding it to the previous number
-            if(nums[j] != nums[i]){
-                i++;
+            if(nums[j] != nums[j - 1]){
                 nums[i] = nums[j];
+                i++;
             }
+
+            j++;
         }
         
         //length of the new array of distinct element 
-        return i+1;
+        return i;
     }
 };
