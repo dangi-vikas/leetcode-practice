b141e10d9725df66a759ae0b9512a42c1391d94a
dangi-vikas <mvikasdangi@gmail.com>
2022-11-14 19:33:12 +0530

    Most Stones Removed with Same Row or Column

--- problems/947-most-stones-removed-with-same-row-or-column.cpp
+++ problems/947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,63 +1,52 @@
-class Solution {
-    vector<int> parent, rank;
-    int islands;
+class UnionFind {
+    unordered_map<int, int> parent, rank;
     
 public:
+    int groups;
+    
+    UnionFind(vector<vector<int>> stones) {
+        for(vector<int> stone : stones) {
+            int row = -(stone[0] + 1);
+            int col = stone[1] + 1;
+            parent[row] = row;
+            parent[col] = col;
+        }
+        
+        groups = parent.size();
+    }
+    
     int _find(int x) {
         if(parent[x] == x) return x;
         return parent[x] = _find(parent[x]);
     }
     
-    void _union(int x, int y){
+    void _union(int x, int y) {
         int px = _find(x), py = _find(y);
         
         if(px != py) {
             if(rank[px] < rank[py]) parent[px] = py;
-            
             else {
                 parent[py] = px;
                 if(rank[px] == rank[py]) rank[px]++;
             }
             
-            islands--;
-        }
+            groups--;
+        } 
     }
-    
+};
+
+class Solution {
+public:
     int removeStones(vector<vector<int>>& stones) {
-        int n = stones.size();
+        UnionFind uf(stones);
         
-        parent.resize(n);
-        rank.resize(n);
-        islands = n;
-        
-        for(int i=0; i<n; i++){
-            parent[i] = i;
-            rank[i] = 1;
-        }
-        
-        unordered_map<int, unordered_set<int>> mapx;
-        unordered_map<int, unordered_set<int>> mapy;
-        
-        for(int i=0; i<stones.size(); i++){
-            mapx[stones[i][0]].insert(i);
-            mapy[stones[i][1]].insert(i);
-        }
-        
-        for(auto itr : mapx) {
-            int grpLead = *itr.second.begin();
+        for(vector<int> stone : stones) {
+            int row = -(stone[0] + 1);
+            int col = stone[1] + 1;
             
-            for(auto it : itr.second)
-                _union(grpLead, it);
+            uf._union(row, col);
         }
         
-        
-        for(auto itr : mapy) {
-            int grpLead = *itr.second.begin();
-            
-            for(auto it : itr.second)
-                _union(grpLead, it);
-        }
-        
-        return n - islands;
+        return stones.size() - uf.groups;
     }
 };
