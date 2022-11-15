3b760c88ae1286a6181f81d1c739f02143c4f484
dangi-vikas <mvikasdangi@gmail.com>
2022-11-15 20:02:37 +0530

    Count Complete Tree Nodes

--- problems/222-count-complete-tree-nodes.cpp
+++ problems/222-count-complete-tree-nodes.cpp
@@ -9,41 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     int countNodes(TreeNode* root) {
         if(!root) return 0;
         
-        int lh = findHeightLeft(root);
-        int rh = findHeightRight(root);
+        
+        int leftHeight = findHeight(root->left, 0);
+        int rightHeight = findHeight(root->right, 1);
         
         /*if left height and right height is equal
           then the tree is perfect*/
-        if(lh == rh) return (1<<lh) -1;
+        if(leftHeight == rightHeight)
+            return (pow(2, leftHeight+1) -1);
+        
         
         //root + nodes on left + nodes on right
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
     
-    //finding the left subtree height
-    int findHeightLeft(TreeNode* root){
-        int height = 0;
-        while(root){
-            height++;
-            root = root->left;
-        }
+    //finding the right or left subtree height
+    int findHeight(TreeNode* root, int isRight) {
+        if(!root) return 0;
         
-        return height;
-    }
-    
-    //finding the right subtree height
-    int findHeightRight(TreeNode* root){
-        int height = 0;
-        while(root){
-            height++;
-            root = root->right;
-        }
-
-        return height;
+        if(isRight) return 1 + findHeight(root->right, 1);
+        
+        return 1 + findHeight(root->left, 0);
     }
 };
