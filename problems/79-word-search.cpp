e36c4587f9186f741e960faef2b1bcabd0658cf1
dangi-vikas <mvikasdangi@gmail.com>
2022-11-24 22:55:10 +0530

    Word Search

--- problems/79-word-search.cpp
+++ problems/79-word-search.cpp
@@ -1,43 +1,36 @@
 class Solution {
+    vector<int> dx = {-1, 0, 1, 0};
+    vector<int> dy = {0, -1, 0, 1};
+    
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size(), n=board[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        int n = board.size(), m = board[0].size();
         
-        //iterating through the matrix
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
-
-                //initiating BFS
-                if(board[i][j] == word[0] && bfs(board, i, j, word, 0, visited))
+        for(int i=0; i<n; i++) 
+            for(int j=0; j<m; j++) 
+                if(board[i][j] == word[0] && dfs(board, word, n, m, i, j, 0))
                     return true;
-            
+        
         return false;
     }
-                         
-    bool bfs(vector<vector<char>>& board, int i, int j, string word, int index,
-                vector<vector<bool>>& visited){
+    
+    bool dfs(vector<vector<char>>& board, string& word, int n, int m, int row, int col, int index) {
+        if(index == word.length()-1) 
+            return true;
         
-        //if all the characters in the word string are checked 
-        if(index==word.size()-1) return true;
+        char temp = board[row][col];
+        board[row][col] = '$';
         
-        //marking visited for current character
-        visited[i][j] = true;
+        for(int d=0; d<4; d++) {
+            int x = row + dx[d];
+            int y = col + dy[d];
+            
+            if(x >= 0 && y >= 0 && x < n && y < m && board[x][y] == word[index+1])
+                if(dfs(board, word, n, m, x, y, index+1)) return true;
+        }
         
-        //checking in all the direction for the next character
-        if(i>0 && !visited[i-1][j] && board[i-1][j]==word[index+1] && bfs(board, i-1, j,
-           word, index+1, visited)) return true;
-        if(j>0 && !visited[i][j-1] && board[i][j-1]==word[index+1] && bfs(board, i, j-1,
-           word, index+1, visited)) return true;
-        if(i<board.size()-1 && !visited[i+1][j] && board[i+1][j]==word[index+1] &&
-           bfs(board, i+1, j, word, index+1, visited)) return true;
-        if(j<board[0].size()-1 && !visited[i][j+1] && board[i][j+1]==word[index+1] &&
-           bfs(board, i, j+1, word, index+1, visited)) return true;
+        board[row][col] = temp;
         
-        //backtracking step: marking the current charcter as unvisited if the word is not found
-        visited[i][j] = false;
-        
-        //if the word is not found 
-        return false;
+        return false; 
     }
 };
