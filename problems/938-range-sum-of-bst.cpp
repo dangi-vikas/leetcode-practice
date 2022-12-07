6200e708040f4497843b3cc360fe3ecc68ed681b
dangi-vikas <mvikasdangi@gmail.com>
2022-12-07 20:25:43 +0530

    Range Sum of BST

--- problems/938-range-sum-of-bst.cpp
+++ problems/938-range-sum-of-bst.cpp
@@ -9,15 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
+ 
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(!root) return 0;
-        
-        if(root->val >= low && root->val <= high)
+
+        if(root->val >= low && root->val <= high) 
             return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
-        
+
         return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
     }
 };
