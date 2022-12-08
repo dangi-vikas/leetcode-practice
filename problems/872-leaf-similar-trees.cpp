cbc808a3f2dcbb9bb9cb8ca05221dd37493744d6
dangi-vikas <mvikasdangi@gmail.com>
2022-12-08 19:59:20 +0530

    Leaf-Similar Trees

--- problems/872-leaf-similar-trees.cpp
+++ problems/872-leaf-similar-trees.cpp
@@ -9,26 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
+ 
 class Solution {
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        if(!root1 && !root2) return true;
-        if(!root1 || !root2) return false;
-        
         vector<int> leaves1, leaves2;
-        
-        setLeafArray(root1 , leaves1);        
-        setLeafArray(root2 , leaves2);
-		
+
+        findLeaves(root1, leaves1);
+        findLeaves(root2, leaves2);
+
         return leaves1 == leaves2;
     }
-    
-    void setLeafArray(TreeNode* root, vector<int>& leaves) {
-        if(!root->left && !root->right) 
-            leaves.push_back(root->val);        
-        
-        if(root->left) setLeafArray(root->left, leaves);
-        if(root->right) setLeafArray(root->right, leaves);
+
+    void findLeaves(TreeNode* root, vector<int>& leaves) {
+        if(!root) return;
+
+        if(!root->left && !root->right) {
+            leaves.push_back(root->val);
+            return;
+        }
+
+        findLeaves(root->left, leaves);
+        findLeaves(root->right, leaves);
     }
 };
