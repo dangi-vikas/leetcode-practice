ff92d0944edec745522faf8577c1011575a8cff6
Vikas Dangi <mvikasdangi@gmail.com>
2022-12-11 16:19:57 +0530

    Binary Tree Maximum Path Sum

---
+++ problems/**  * Definition for a binary tree node.  * struct TreeNode {  *     int val;  *     TreeNode *left;  *     TreeNode *right;  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}  * };  */124-binary-tree-maximum-paths-sum.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int maxPathSum(TreeNode* root) {
+        int maxSum = INT_MIN;   
+        maxPathSum(root, maxSum);
+        return maxSum;
+    }
+
+    int maxPathSum(TreeNode* root, int& maxSum) {
+        if(!root) return 0;
+
+        int leftSum = max(maxPathSum(root->left, maxSum), 0);
+        int rightSum = max(maxPathSum(root->right, maxSum), 0);
+
+        maxSum = max(maxSum, (leftSum + rightSum + root->val));
+
+        return max(leftSum, rightSum) + root->val;
+    }
+};
