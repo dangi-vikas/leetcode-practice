1243e9f56440276ad251154dbc3f686f9096e32b
dangi-vikas <mvikasdangi@gmail.com>
2022-12-12 19:15:54 +0530

    Climbing Stairs

--- problems/70-climbing-stairs.cpp
+++ problems/70-climbing-stairs.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        if(n == 1) return 1;
 
-        //if n is one 
-        if(n==1) return 1;
-        
-        int first=1, second=2;
-        
-        for(int i=3;i<=n;i++){
-            int third=second+first;
-            first=second;
-            second=third;
+        int first = 1, second = 2;
+
+        for(int i=3; i<=n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
         }
-        
+
         return second;
     }
 };
