8445ab7bf69682c6c8ba960459d7a93ec1e9d147
dangi-vikas <mvikasdangi@gmail.com>
2022-12-13 23:00:45 +0530

    Minimum Falling Path Sum

--- problems/931-minimum-falling-path-sum.cpp
+++ problems/931-minimum-falling-path-sum.cpp
@@ -1,30 +1,41 @@
+931. Minimum Falling Path Sum
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
-        vector<int> prev(m, 0);
-        
-        int minSum = 1e8;
-        
-        for(int i=0; i<m; i++) prev[i] = matrix[0][i];
-        
-        for(int i=1; i<n; i++) {
-            vector<int> curr(m, 0);
-            
-            for(int j=0; j<m; j++) {
-                int u = matrix[i][j] + prev[j];
-                int ld = matrix[i][j] + (j-1>=0 ? prev[j-1] : 1e8);
-                int rd = matrix[i][j] + (j+1<m ? prev[j+1] : 1e8);
-                
-                curr[j] = min(u, min(ld, rd));
+        int n = matrix.size();
+        int ans = INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int c=0; c<n; c++) dp[n-1][c] = matrix[n-1][c];
+
+        for(int i=n-2; i>=0; i--) {
+            for(int j=0; j<n; j++) {
+                int leftDiag = INT_MAX, down = INT_MAX, rightDiag = INT_MAX;
+
+                if(j > 0) leftDiag = dp[i+1][j-1];
+                down = dp[i+1][j];
+                if(j < n-1) rightDiag = dp[i+1][j+1];
+
+                dp[i][j] = matrix[i][j] + min(leftDiag, min(down, rightDiag));
             }
-            
-            prev = curr;
         }
-        
-        for(int i=0; i<m; i++) 
-            minSum = min(minSum, prev[i]);
-        
-        return minSum;
+
+        for(int c=0; c<n; c++) 
+            ans = min(ans, dp[0][c]);
+       
+        return ans;
+    }
+
+    int minFallingPathSumCol(vector<vector<int>>& matrix, int m, int n, int row, int col, vector<vector<int>>& dp) {
+        int leftDiag = INT_MAX, down = INT_MAX, rightDiag = INT_MAX;
+
+        if(row == n) return 0;
+        if(dp[row][col] != -1) return dp[row][col];
+
+        if(col > 0) leftDiag = minFallingPathSumCol(matrix, m, n, row+1, col-1, dp);
+        down = minFallingPathSumCol(matrix, m, n, row+1, col, dp);
+        if(col < m-1) rightDiag = minFallingPathSumCol(matrix, m, n, row+1, col+1, dp);
+
+        return dp[row][col] = matrix[row][col] + min(leftDiag, min(down, rightDiag));
     }
 };
