e4e9e06748478cf298d42b4efc2920c2059c4153
dangi-vikas <mvikasdangi@gmail.com>
2022-12-14 22:22:53 +0530

    Minimum Falling Path Sum

--- problems/931-minimum-falling-path-sum.cpp
+++ problems/931-minimum-falling-path-sum.cpp
@@ -1,27 +1,30 @@
-931. Minimum Falling Path Sum
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int ans = INT_MAX;
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> next(n, 0);
 
-        for(int c=0; c<n; c++) dp[n-1][c] = matrix[n-1][c];
+        for(int c=0; c<n; c++) next[c] = matrix[n-1][c];
 
         for(int i=n-2; i>=0; i--) {
+            vector<int> curr(n, 0);
+
             for(int j=0; j<n; j++) {
                 int leftDiag = INT_MAX, down = INT_MAX, rightDiag = INT_MAX;
 
-                if(j > 0) leftDiag = dp[i+1][j-1];
-                down = dp[i+1][j];
-                if(j < n-1) rightDiag = dp[i+1][j+1];
+                if(j > 0) leftDiag = next[j-1];
+                down = next[j];
+                if(j < n-1) rightDiag = next[j+1];
 
-                dp[i][j] = matrix[i][j] + min(leftDiag, min(down, rightDiag));
+                curr[j] = matrix[i][j] + min(leftDiag, min(down, rightDiag));
             }
+
+            next = curr;
         }
 
         for(int c=0; c<n; c++) 
-            ans = min(ans, dp[0][c]);
+            ans = min(ans, next[c]);
        
         return ans;
     }
