d74453cb1de26f15fed403bc6d42056d143ec412
dangi-vikas <mvikasdangi@gmail.com>
2022-12-24 23:15:41 +0530

    Domino and Tromino Tilling

--- problems/790-domino-and-tromino-tiling.cpp
+++ problems/790-domino-and-tromino-tiling.cpp
@@ -1,17 +1,17 @@
 class Solution {
-    int mod = 1000000007;
+    int mod = 1e9 + 7;
+
 public:
     int numTilings(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        
         vector<int> dp(n+1);
-        
-        dp[1] = 1; dp[2] = 2; dp[3] = 5;
-        
-        for(int i=4; i<=n; i++) 
-            dp[i] = ((2 * dp[i-1]) % mod + dp[i-3] % mod) % mod;
-        
+
+        if(n < 3) return n;
+
+        dp[0] = 1; dp[1] = 1; dp[2] = 2;
+
+        for(int i=3; i<=n; i++) 
+            dp[i] = ((2 * dp[i-1]) % mod + dp[i-3]) % mod;
+
         return dp[n];
     }
 };
