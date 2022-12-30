1c82a6c5ff0af3cd3172e28acf84e5cb479c49e8
dangi-vikas <mvikasdangi@gmail.com>
2022-12-30 19:56:33 +0530

    All Paths From Source to Target

--- problems/797-all-paths-from-source-to-target.cpp
+++ problems/797-all-paths-from-source-to-target.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> ans;
-        vector<int> path;
-        
-        dfs(graph, path, ans, 0);
-        
+        vector<int> container;
+
+        dfs(graph, ans, 0, container);
+
         return ans;
     }
-    
-    void dfs(vector<vector<int>>& graph, vector<int>& path, vector<vector<int>>& ans, int src) {
-        path.push_back(src);
-        
-        if(src == graph.size() - 1) 
-            ans.push_back(path); 
-        
-        else 
-            for(int neighbor : graph[src]) dfs(graph, path, ans, neighbor);
-        
-        path.pop_back(); 
+
+    void dfs(vector<vector<int>>& graph, vector<vector<int>>& ans, int src, vector<int>& container) {
+        container.push_back(src);
+
+        if(src ==  graph.size() - 1) {
+            ans.push_back(container);
+            return;
+        }
+
+        for(auto nbr : graph[src]) {
+            dfs(graph, ans, nbr, container);
+            container.pop_back();
+        }
     }
 };
