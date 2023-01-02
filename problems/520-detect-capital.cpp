8bf4b760a4707dadbce946db1214c2a20306bd0c
dangi-vikas <mvikasdangi@gmail.com>
2023-01-02 22:22:13 +0530

    Detect Capital

--- problems/520-detect-capital.cpp
+++ problems/520-detect-capital.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        for (int i = 1; i < word.size(); i++){
-            if (islower(word[i-1]) && isupper(word[i])) return false;
-            else if (isupper(word[i-1]) && islower(word[i]) && i!=1) return false;
-        } 
-            
+
+        for(int i=0; i<word.length(); i++) {
+            if(islower(word[0]) && isupper(word[i])) return false;
+            if(i > 1 && isupper(word[0]) && islower(word[1]) && isupper(word[i])) return false;
+            if(i > 1 &&isupper(word[0]) && isupper(word[1]) && islower(word[i])) return false;
+        }
+
         return true;
     }
 };
