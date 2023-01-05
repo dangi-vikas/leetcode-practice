a6211a3545720fe6c159ff1e5d5ff5ae3d2a56ae
dangi-vikas <mvikasdangi@gmail.com>
2023-01-05 20:38:54 +0530

    Minimum Number of Arrows to Burst Balloons

--- problems/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ problems/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end());
-        
+
         int end = points[0][1];
-        int count = 1;
-        
-        for(int i=1; i<points.size(); i++){
+        int ans = 1;
+
+        for(int i=1; i<points.size(); i++) {
             if(points[i][0] <= end)
                 end = min(end, points[i][1]);
             
-            else{
-                count++;
+            else {
+                ans++;
                 end = points[i][1];
             }
         }
-        
-        return count;
+
+        return ans;
     }
 };
