092996cdc3c6238a411f649b1743aaf49b35060b
dangi-vikas <mvikasdangi@gmail.com>
2023-01-15 00:00:34 +0530

    Lexicographically Smallest Equivalent String

--- problems/1061-lexicographically-smallest-equivalent-string.cpp
+++ problems/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,29 +1,36 @@
+class UnionFind {
+public:
+    vector<int> parent;
+
+    UnionFind() {
+        parent = vector<int>(26, -1);
+    }
+    
+    int findParent(int node){
+        if(parent[node] == -1) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionBySize(int x, int y) {
+        int px = findParent(x);
+        int py = findParent(y);
+        
+        if (px != py) 
+            parent[max(px, py)] = min(px, py); 
+    }
+};
+
 class Solution {
 public:
-    int par[26];
-    
-    int find(int x){
-        if(par[x]==-1) return x;
-        return par[x]=find(par[x]);
-    }
-    
-    void Union(int x, int y) {
-        x = find(x);
-        y = find(y);
-        
-        if (x != y) 
-            par[max(x, y)] = min(x, y); 
-    }
-	
     string smallestEquivalentString(string s1, string s2, string baseStr) {
         
-        memset(par, -1, sizeof(par));
+        UnionFind uf = UnionFind();
         
-        for (auto i = 0; i < s1.size(); ++i) 
-            Union(s1[i] - 'a', s2[i] - 'a');
+        for (auto i=0; i<s1.size(); ++i) 
+            uf.unionBySize(s1[i] - 'a', s2[i] - 'a');
         
-        for(auto i=0;i<baseStr.size();i++) 
-            baseStr[i]=find(baseStr[i]-'a')+'a';
+        for(auto i=0; i<baseStr.size(); i++) 
+            baseStr[i] = uf.findParent(baseStr[i] - 'a') + 'a';
 
         return baseStr;
     }
