2071fabd437109fd8ea98279580ef30bba0e3712
dangi-vikas <mvikasdangi@gmail.com>
2023-01-16 23:34:21 +0530

    Insert Interval

--- problems/57-insert-interval.cpp
+++ problems/57-insert-interval.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> ans;
         
         //if intervals is empty
-        if(intervals.size()==0){
+        if(intervals.size() == 0){
             ans.push_back(newInterval);  
             return ans;
         } 
@@ -15,31 +15,29 @@ public:
         for(int i=0; i<intervals.size(); i++){
 
             //if the newInterval is subset of one of the intervals of (intervals vector)
-            if(intervals[i][1]>=newInterval[0]){
-                int j=i;
-                int minI=newInterval[0];
-                int maxI=newInterval[1];
+            if(intervals[i][1] >= newInterval[0]){
+                int j = i;
+                int minI = newInterval[0];
+                int maxI = newInterval[1];
                 
-                while(j<intervals.size() && intervals[j][0]<=newInterval[1]){
-                    minI=min(intervals[j][0],minI);
-                    maxI=max(intervals[j][1],maxI);
+                while(j < intervals.size() && intervals[j][0] <= newInterval[1]){
+                    minI = min(intervals[j][0], minI);
+                    maxI = max(intervals[j][1], maxI);
                     j++;
                 }
                 
                 //pushing the interval with accomodating extremes 
-                ans.push_back({minI,maxI});
+                ans.push_back({minI, maxI});
                 isMerged = true;
                 
-                while(j<intervals.size()){
+                while(j < intervals.size()){
                     ans.push_back(intervals[j]);
                     j++;
                 }
-                break;
 
+                break;
             }
-            else{
-                ans.push_back(intervals[i]);
-            }
+            else ans.push_back(intervals[i]);
         }
 
         //if newInterval is greater than every interval
