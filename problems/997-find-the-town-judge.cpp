b63f28554f1e76c0d1350570af07e4dccbaed5eb
dangi-vikas <mvikasdangi@gmail.com>
2023-01-23 21:17:33 +0530

    Find the Town Judge

--- problems/997-find-the-town-judge.cpp
+++ problems/997-find-the-town-judge.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> counts(n, 0);
-        int len = trust.size();
-        
-        for(int i=0; i<len; i++) {
-            counts[trust[i][0] - 1]--;
-            counts[trust[i][1] - 1]++;
+        int size = trust.size();
+        vector<int> trustCount(n, 0);
+
+        for(int i=0; i<size; i++) {
+            trustCount[trust[i][0] - 1]--;
+            trustCount[trust[i][1] - 1]++;
         }
-        
-        for(int i=0; i<n; i++)
-            if(counts[i] == n-1) return i+1;
-        
-        return -1; 
+
+        for(int i=0; i<n; i++) 
+            if(trustCount[i] == n-1) return i+1;
+
+        return -1;
     }
 };
