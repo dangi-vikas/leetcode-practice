21705e6c4477dbf667ee7e9054edc7a0042f9259
dangi-vikas <mvikasdangi@gmail.com>
2023-01-26 20:29:54 +0530

    Cheapest Flights Within K Stops

--- problems/787-cheapest-flights-within-k-stops.cpp
+++ problems/787-cheapest-flights-within-k-stops.cpp
@@ -1,43 +1,44 @@
-    class Solution {
+class Solution {
     int minPrice = INT_MAX;
-    
+
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<int>> dp(n, vector<int>(n));
-        
-        for(auto flight : flights)
+        queue<pair<int, int>> q;
+        vector<int> visited(n, INT_MAX);
+
+        for(auto flight : flights) 
             dp[flight[0]][flight[1]] = flight[2];
         
-        queue<vector<int>> q;
         q.push({src, 0});
-        
-        vector<int> visited(n, minPrice);
-        
-        while(k>=0 && !q.empty()) {
-            k--;
+
+        while(k >= 0 && !q.empty()) {
             int size = q.size();
-            
+            k--;
+
             for(int i=0; i<size; i++) {
-                vector<int> pair = q.front();
+                int source = q.front().first;
+                int price = q.front().second;
+                vector<int> nbrs = dp[source];
+
                 q.pop();
-                int source = pair[0];
-                int price = pair[1];
-                vector<int> next = dp[source];
-                
-                for(int d=0; d<n; d++) {
-                    if(next[d]) {
-                        int p = next[d] + price;
-                        if(d != dst && visited[d] > p) {
-                            q.push({d, p});
-                            visited[d] = p;
+
+                for(int node=0; node<n; node++) {
+                    if(nbrs[node]) {
+                        int dis = nbrs[node] + price;
+
+                        if(node != dst && visited[node] > dis) {
+                            q.push({node, dis});
+                            visited[node] = dis;
                         }
-                        
-                        else if(d == dst) minPrice = min(minPrice, p);
-                    }    
+
+                        else if(node == dst) 
+                            minPrice = min(minPrice, dis);
+                    }
                 }
             }
         }
-        
+
         return minPrice == INT_MAX ? -1 : minPrice;
     }
 };
