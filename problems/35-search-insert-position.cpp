b51b0252cac621501acf372971d1e57d3eb351c4
dangi-vikas <mvikasdangi@gmail.com>
2023-02-20 20:18:25 +0530

    Search Insert Position

--- problems/35-search-insert-position.cpp
+++ problems/35-search-insert-position.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int low=0, high=nums.size()-1;
+        int low = 0, high = nums.size()-1;
 
         //Binary Search      
-        while(high>=low){
-            int mid = low + ((high-low)>>2);
+        while(high >= low){
+            int mid = low + ((high-low) >> 1);
             
             if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) high = mid-1;
-            else low = mid+1;
+            if(nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
         }
         
         /*the insert position will be low because it 
