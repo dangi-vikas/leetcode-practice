5c84053d187863355236b714c78b98e66844a9f2
dangi-vikas <mvikasdangi@gmail.com>
2023-02-21 22:32:21 +0530

    Single Element in Sorted Array

--- problems/540-single-element-in-sorted-array.cpp
+++ problems/540-single-element-in-sorted-array.cpp
@@ -3,8 +3,7 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int xOr =  nums[0];
         
-        for(int i=1; i<nums.size(); i++)
-            xOr ^= nums[i];
+        for(int i=1; i<nums.size(); i++) xOr ^= nums[i];
         
         return xOr;
     }
