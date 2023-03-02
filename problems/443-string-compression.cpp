4ee0763a08a81d6d09b1e192133ea5450ff04bb5
dangi-vikas <mvikasdangi@gmail.com>
2023-03-02 23:46:44 +0530

    String Compression

--- problems/443-string-compression.cpp
+++ problems/443-string-compression.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int i = 0 ; 
-        int validIndex = 0 ; 
+        int i = 0;
+        int validIndex = 0;
         int n = chars.size();
-        
-        while(i < n){
-            int j = i + 1; 
-            
+
+        while(i < n) {
+            int j = i + 1;
+
             while(j < n && chars[i] == chars[j]) j++;
-            
+
             chars[validIndex++] = chars[i];
-            int count =  j-i;
-            
-            if(count > 1){
-                string cstring = to_string(count); 
-                for(char ch : cstring)
-                    chars[validIndex++] = ch; 
+            int count = j - i;
+
+            if(count > 1) {
+                string countString = to_string(count);
+                for(char ch : countString) chars[validIndex++] = ch;
             }
-            
+
             i = j;
         }
-        
-        return validIndex;
+
+        return validIndex;  
     }
+
+
 };
