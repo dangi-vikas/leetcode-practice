f9fd3f0374da5917bf44d7d7cfc8bd82b041523d
Vikas Dangi <mvikasdangi@gmail.com>
2023-03-06 22:27:55 +0530

    Kth Missing Positive Number

---
+++ problems/1539-kth-missing-positive-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int low = 0, high = arr.size();
+
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if(arr[mid] - mid > k) high = mid;
+            else low = mid + 1;
+        }
+
+        return low + k;
+    }
+};
