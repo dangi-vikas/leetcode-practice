333cd8118289266581eec355c28d8bd9556c12c5
dangi-vikas <mvikasdangi@gmail.com>
2023-03-08 21:50:46 +0530

    Koko Eating Bananas

--- problems/875-koko-eating-bananas.cpp
+++ problems/875-koko-eating-bananas.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1, high = piles[0];
-        
+
         for(int pile : piles) 
             high = max(high, pile);
-        
+
         while(low < high) {
             long mid = low + ((high - low) >> 1);
-            
-            if(!isPossibleToEatAll(piles, h, mid)) low = mid + 1;
+
+            if(!canEatAllBananas(piles, h, mid)) low = mid + 1;
             else high = mid;
         }
-        
+
         return (int) low;
     }
-    
-    bool isPossibleToEatAll(vector<int> piles, int hours, long currK) {
+
+    bool canEatAllBananas(vector<int>& piles, int hours, int currK) {
         long count = 0;
-        
+
         for(long pile : piles) {
             count += pile / currK;
             if(pile % currK) count++;
         }
-        
+
         return count <= hours;
     }
 };
