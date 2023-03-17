10fb57a6ac767df9fa9e7d27c5ff7d27a949b14c
dangi-vikas <mvikasdangi@gmail.com>
2023-03-17 20:03:36 +0530

    Implement Trie (Prefix Tree)

--- problems/208-implement-trie-prefixtree.cpp
+++ problems/208-implement-trie-prefixtree.cpp
@@ -22,7 +22,6 @@ class Trie {
     
 public:
 
-    //initializes the trie object
     Trie() {
           root = getNode('/' - 'a');  
     }
@@ -45,7 +44,6 @@ public:
         curr->endsHere++;
     }
     
-    //returns whether the string word is in the trie (i.e., was inserted before)
     bool search(string word) {
         TrieNode* curr = root;
         int index;
@@ -75,6 +73,91 @@ public:
             curr = curr->child[index];
         }
         return (curr->count > 0);
+    }
+};
+
+
+//BETTER METHOD
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
+
+
+ class TrieNode {
+public:
+    vector<TrieNode*> links = vector<TrieNode*>(26, 0);
+    bool isEnd = false;
+
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != nullptr;
+    }
+
+    void put(char ch, TrieNode* node) {
+        links[ch - 'a'] = node;
+    }
+
+    TrieNode* get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void setEnd() {
+        isEnd = true;
+    }
+};
+
+class Trie {
+    TrieNode* root;
+
+public:
+    //initializes the trie object
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* node = root;
+        
+        for(int i=0; i<word.length(); i++) {
+            if(!node->containsKey(word[i]))    
+                node->put(word[i], new TrieNode());
+            
+            node = node->get(word[i]);
+        }
+
+        node->setEnd();
+    }
+
+    //returns whether the string word is in the trie (i.e., was inserted before)
+    bool search(string word) {
+        TrieNode* node = root;
+
+        for(int i=0; i<word.length(); i++) {
+            if(!node->containsKey(word[i]))
+                return false;
+
+            node = node->get(word[i]);
+        }
+
+        return node->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+
+        for(int i=0; i<prefix.length(); i++) {
+            if(!node->containsKey(prefix[i]))
+                return false;
+
+            node = node->get(prefix[i]);
+        }
+
+        return true;
     }
 };
 
