3714627521e2ab057649218819cf4d0efff7abbf
dangi-vikas <mvikasdangi@gmail.com>
2023-03-20 19:53:42 +0530

    Can Place Flowers

--- problems/605-can-place-flowers.cpp
+++ problems/605-can-place-flowers.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int len = flowerbed.size();
-        
+
         if(n == 0) return true;
 
-        for(int i=0; i<len && n>0; ++i) {
-            if(flowerbed[i] == 1) {
-                ++i; continue;
-            }
-            
-            int next = (i == len-1) ? 0 : flowerbed[i+1];
-            if(next == 0) {
-                --n; ++i; 
-            }
+        for(int i=0; i<len && n>0; i++) {
+            if(flowerbed[i] == 1) i++;
+
+            else {
+                int next = (i == len-1) ? 0 : flowerbed[i+1];
+                
+                if(next == 0) {
+                    n--; 
+                    i++;
+                }
+            } 
         }
 
         return n == 0;
