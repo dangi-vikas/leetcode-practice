d17084e9e0ae41bd8e7557b273cd5b4b83f8330b
dangi-vikas <mvikasdangi@gmail.com>
2023-04-24 21:05:37 +0530

    Last Stone Weight

--- problems/1046-last-stone-weight.cpp
+++ problems/1046-last-stone-weight.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int, vector<int>> pq;
-        int lastWeight = 0;
-        
+        priority_queue<int> pq;
+
         for(int stone : stones) pq.push(stone);
-        
-        while(pq.size() > 1) {
-            int first = pq.top();
+
+        while(!pq.empty()) {
+            int max = pq.top();
             pq.pop();
+
+            if(pq.size() == 0) return max;
             
-            int second = pq.top();
+            int secondMax = pq.top();
             pq.pop();
-            
-            if(first == second && pq.size() == 0) return 0;
-            
-            if(first < second || second < first) 
-                pq.push(abs(first-second));
+
+            cout<<max<<secondMax;
+
+            if(max != secondMax) pq.push(max - secondMax);
         }
-        
-        return pq.top();
+
+        return 0;
     }
 };
