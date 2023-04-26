9ed4ae4627661d4f2de309de7a278e153b041903
dangi-vikas <mvikasdangi@gmail.com>
2023-04-26 20:08:06 +0530

    Add Digits

--- problems/258-add-digits.cpp
+++ problems/258-add-digits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int addDigits(int num) {
         if(num == 0) return 0;
-        if(!(num%9)) return 9;
-        return num%9;
+        if(!(num % 9)) return 9;
+        return num % 9;
     }
 };
