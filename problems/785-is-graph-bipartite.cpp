c88255ee6e1b6087c1eacd2e4be9eb2a48ca56b4
dangi-vikas <mvikasdangi@gmail.com>
2023-05-19 20:29:22 +0530

    Is Graph Bipartite

--- problems/785-is-graph-bipartite.cpp
+++ problems/785-is-graph-bipartite.cpp
@@ -2,36 +2,34 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n+1, -1);
-        
+        vector<int> color(n, -1);
+
         for(int i=0; i<n; i++) 
-            if(color[i] == -1) 
-                if(!dfs(graph, i, color)) return false;
-        
-        return true;     
+            if(!isColored(0, i, color, graph))
+                return false;
+
+        return true;
     }
-    
-    bool dfs(vector<vector<int>>& graph, int& src, vector<int>& color) {
-        color[src] = 1;
-        
+
+    bool isColored(int src, int dst, vector<int>& color, vector<vector<int>>& graph) {
         queue<int> q;
-        q.push(src);
+
+        q.push(dst);
         
         while(!q.empty()) {
-            int s = q.front();
+            int node = q.front();
             q.pop();
-            
-            for(auto neighbor : graph[s]) {
-                if(color[neighbor] == -1) {
-                    color[neighbor] = 1 - color[s];
-                    q.push(neighbor);
+
+            for(auto nbr : graph[node]) {
+                if(color[nbr] == -1) {
+                    color[nbr] = !color[node];
+                    q.push(nbr);
                 }
-                
-                else if(color[neighbor] == color[s])
-                    return false;
+
+                else if(color[nbr] == color[node]) return false;
             }
         }
-        
+
         return true;
     }
 };
