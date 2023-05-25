f0244208ae9bb3b327a02e5b4b8b83a29e5850d2
dangi-vikas <mvikasdangi@gmail.com>
2023-05-25 20:59:09 +0530

    New 21 Game

--- problems/837-new-21-game.cpp
+++ problems/837-new-21-game.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     double new21Game(int n, int k, int maxPts) {
+        if(k == 0 || n >= k + maxPts) return 1.0;
+
         vector<double> dp(k + maxPts, 0);
-        
-        for(int i=k; i<=min(k+maxPts-1, n); i++) 
-            dp[i] = 1;
-        
-        double sum = min(n - k + 1, maxPts); 
-        
-        for(int i=k-1; i>=0; i--) {
-            dp[i] = sum * (1 / (float) maxPts);
-            sum = sum - dp[i + maxPts] + dp[i]; 
+        double sum = 1.0;
+        double ans = 0.0;
+
+        dp[0] = 1.0;
+
+        for(int i=1; i<=n; i++) {
+            dp[i] = sum * (1 / (double) maxPts);
+
+            if(i < k) sum += dp[i];
+            else ans += dp[i];
+            if(i - maxPts >= 0) sum -= dp[i - maxPts]; 
         }
-        
-        return dp[0];
+
+        return ans;
     }
 };
