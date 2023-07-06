ce2c0d54c045629de8eef11e88af03de1d70b7a7
dangi-vikas <mvikasdangi@gmail.com>
2023-07-06 22:01:30 +0530

    Minimum Size Subarray Sum

--- problems/209-minimum-size-subarray-sum.cpp
+++ problems/209-minimum-size-subarray-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int sum=0, i=0, j=0, len = INT_MAX;
-        
+         
         while(j<nums.size()){
             sum += nums[j++];
             
