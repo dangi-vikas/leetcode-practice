b934f9ea0968dbf8d69f5eda6b92eaeba1c41fb7
dangi-vikas <mvikasdangi@gmail.com>
2023-07-12 21:37:12 +0530

    Find Evventual Safe States

--- problems/802-find-eventual-safe-states.cpp
+++ problems/802-find-eventual-safe-states.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        unordered_map<int, bool> safe;
+        int n = graph.size();
+        vector<vector<int>> adjList(n);
+        vector<int> inDegree(n);
+        queue<int> q;
         vector<int> ans;
-        
-        for(int i=0; i<graph.size(); i++) 
-            if(dfs(graph, safe, i)) 
-                ans.push_back(i);
-        
+
+        for(int i=0; i<n; i++) {
+            for(int j : graph[i]) {
+                adjList[j].push_back(i);
+                inDegree[i]++;
+            }
+        }
+
+        for(int i=0; i<n; i++) 
+            if(inDegree[i] == 0) q.push(i);
+
+        while(!q.empty()) {
+            int src = q.front();
+            q.pop();
+
+            ans.push_back(src);
+
+            for(int nbr : adjList[src]) {
+                inDegree[nbr]--;
+                if(inDegree[nbr] == 0) q.push(nbr);
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
         return ans;
-    }
-    
-    bool dfs(vector<vector<int>>& graph, unordered_map<int, bool>& safe, int& src) {
-        if(safe.find(src) != safe.end()) 
-            return safe[src];
-        
-        safe[src] = false;
-        
-        for(auto neighbour : graph[src]) 
-            if(!dfs(graph, safe, neighbour))
-               return safe[src];
-               
-        safe[src] = true;
-        return safe[src];
     }
 };
