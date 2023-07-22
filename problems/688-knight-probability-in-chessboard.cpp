71445fec0b6eb75d24cba34a4d6e1b730aac2424
dangi-vikas <mvikasdangi@gmail.com>
2023-07-22 15:45:27 +0530

    Knight Probablity in Chess

--- problems/688-knight-probability-in-chessboard.cpp
+++ problems/688-knight-probability-in-chessboard.cpp
@@ -1,34 +1,28 @@
 class Solution {
     double dp[26][26][101];
-    
+    vector<int> dx = {2, -2, 2, -2, 1, -1, 1, -1};
+    vector<int> dy = {-1, -1, 1, 1, -2, -2, 2, 2}; 
+
 public:
     double knightProbability(int n, int k, int row, int column) {
         memset(dp, 0.0, sizeof(dp));
-        return knightProbabilityRec(n, k,  row, column);
+        return knightProbablityRec(n, k, row, column);
     }
-    
-    double knightProbabilityRec(int n, int k, int i, int j){
-        if(i >= n || j >= n || i < 0 || j < 0 || k < 0) return 0;
+
+    double knightProbablityRec(int n, int k, int r, int c) {
+        if(r < 0 || c < 0 || r >= n || c >= n || k < 0) return 0;
         if(k == 0) return 1;
-        if(dp[i][j][k] != 0) return dp[i][j][k];
+        if(dp[r][c][k] != 0) return dp[r][c][k];
+
+        double totalOutcomes = 0.0;
+
+        for(int d=0; d<8; d++) {
+            int x = r + dx[d];
+            int y = c + dy[d];
+            
+            totalOutcomes += knightProbablityRec(n, k-1, x, y); 
+        }
         
-        double total = knightProbabilityRec(n, k - 1, i - 2, j + 1) +
-
-                       knightProbabilityRec(n, k - 1, i - 2, j - 1)  +
-
-                       knightProbabilityRec(n, k - 1, i - 1, j + 2)  +
-
-                       knightProbabilityRec(n, k - 1, i - 1, j - 2)  +
-
-                       knightProbabilityRec(n, k - 1, i + 2, j + 1)  +
-
-                       knightProbabilityRec(n, k - 1, i + 1, j + 2)  +
-
-                       knightProbabilityRec(n, k - 1, i + 2, j - 1)  +
-
-                       knightProbabilityRec(n, k - 1, i + 1, j - 2);
-
-        return dp[i][j][k] = total / 8.0;
-                
+        return dp[r][c][k] = totalOutcomes / 8.0;
     }
 };
