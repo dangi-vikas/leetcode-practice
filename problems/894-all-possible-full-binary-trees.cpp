c778771a1f009179b057474ffac84c8ea033725d
dangi-vikas <mvikasdangi@gmail.com>
2023-07-23 16:21:01 +0530

    All Possible Full Binary Trees

--- problems/894-all-possible-full-binary-trees.cpp
+++ problems/894-all-possible-full-binary-trees.cpp
@@ -9,43 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-    unordered_map<int, vector<TreeNode*>> dp;
-    
 public:
     vector<TreeNode*> allPossibleFBT(int n) {
-        return allPossibleFBTRec(n);
+        unordered_map<int, vector<TreeNode*>> dp;
+        return allPossibleFBT(n, dp);
     }
-    
-    vector<TreeNode*> allPossibleFBTRec(int n) {
+
+    vector<TreeNode*> allPossibleFBT(int n, unordered_map<int, vector<TreeNode*>>& dp) {
         if(n % 2 == 0) return {};
-        
-        if(n == 1) {
-            TreeNode* node = new TreeNode(0);
-            return {node};
-        }
-       
+        if(n == 1) return {new TreeNode(0)};
+
         if(dp.find(n) != dp.end()) return dp[n];
-        
+
         vector<TreeNode*> ans;
-        
-        for(int i = 1; i < n; i+=2) {
-            vector<TreeNode*> left = allPossibleFBTRec(i);
-            vector<TreeNode*> right = allPossibleFBTRec(n-i-1);
-            
+
+        for(int i=1; i<n; i+=2) {
+            vector<TreeNode*> left = allPossibleFBT(i, dp);
+            vector<TreeNode*> right = allPossibleFBT(n-i-1, dp);
+
             for(TreeNode* l : left) {
                 for(TreeNode* r : right) {
                     TreeNode* root = new TreeNode(0);
-                    
-                    root -> left = l;
-                    root -> right = r;
-                    
+
+                    root->left = l;
+                    root->right = r;
+
                     ans.push_back(root);
                 }
             }
         }
-        
+
         return dp[n] = ans;
     }
 };
