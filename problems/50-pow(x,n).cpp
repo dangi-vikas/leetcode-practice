4e4ee88ddfbf3e30c068f6611f0653b5de559dfb
dangi-vikas <mvikasdangi@gmail.com>
2023-07-24 21:23:22 +0530

    Pow(x, n)

--- problems/50-pow(x,n).cpp
+++ problems/50-pow(x,n).cpp
@@ -5,26 +5,24 @@ public:
         long long N = n;            //for accomodating the negative-positive conversion
         
         //taking only positive powers
-        if(N<0) N = -N;
+        if(N < 0) N = -N;
         
-        while(N){
+        while(N) {
 
             //if the power is odd 
-            if(N%2){
+            if(N % 2) {
                 ans *=  x;
                 N--;
             }
 
             //if the power is even
-            else{
+            else {
                 x *= x;           //multiplying with the number itself          
                 N >>= 1;          //halving the power
             }
         }
         
         //in case of negative powers inverting the ans
-        if(n<0) ans = (double)(1.0) /  double(ans);
-
-        return ans;
+        return (n < 0) ? ((double) 1.0 / (double) ans) : ans;
     }
 };
