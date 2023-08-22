e299ae432dd61208261db0d9843fd43d9119f005
dangi-vikas <mvikasdangi@gmail.com>
2023-08-22 20:33:47 +0530

    Excel Sheet Column Title

--- problems/168-excel-sheet-column-title.cpp
+++ problems/168-excel-sheet-column-title.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     string convertToTitle(int columnNumber) {
         string ans = "";
-        while(columnNumber>0){
-            int curr = columnNumber%26;
-            
-            if(curr == 0){
+
+        while(columnNumber) {
+            int rem = columnNumber % 26;
+
+            if(rem == 0) {
                 ans += 'Z';
-                columnNumber = columnNumber/26-1;
+                columnNumber = (columnNumber / 26) - 1;
+            } 
+
+            else {
+                ans += ('A' + rem - 1);
+                columnNumber /= 26;
             }
-            else{
-                ans += ('A'+curr-1);
-                columnNumber = columnNumber/26;
-            }
-            
-        }
+        } 
+
         reverse(ans.begin(), ans.end());
-        
+
         return ans;
     }
 };
