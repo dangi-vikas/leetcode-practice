042d35ed6de4bf765075039005ed0766e3b223f7
Vikas Dangi <mvikasdangi@gmail.com>
2023-09-08 20:35:48 +0530

    Pascal Triangle

--- problems/118-pascal-triangle.cpp
+++ problems/118-pascal-triangle.cpp
@@ -4,20 +4,19 @@ public:
         vector<vector<int>> ans;
         
         //traversing the rows
-        for(int row=1; row<=numRows; row++)
-        {
-            int c=1;
+        for(int row=1; row<=numRows; row++) {
+            int c = 1;
             vector<int> temp;
 
             //traversing the columns
-            for(int i=1; i<=row; i++)
-            {
+            for(int i=1; i<=row; i++) {
                 //pushing the required value
                 temp.push_back(c);
 
                 //updating the value of c for next column to push in
-                c=c*(row-i)/i;
+                c = c * (row-i) / i;
             }
+            
             ans.push_back(temp);
         }
         
