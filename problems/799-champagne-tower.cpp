a6feaf2197ce5b87a76b1b327410aeeb5c29e3a3
Vikas Dangi <mvikasdangi@gmail.com>
2023-09-24 22:06:55 +0530

    Champagne Tower

--- problems/799-champagne-tower.cpp
+++ problems/799-champagne-tower.cpp
@@ -8,10 +8,10 @@ public:
         
         while(query_row--) {
             vector<double> temp;
-            temp.push_back(max((list[0]-1) / 2, 0.0));
+            temp.push_back(max((list[0] - 1) / 2, 0.0));
             
             for(int i=1; i<list.size(); i++) 
-                temp.push_back(max((list[i-1]-1) / 2, 0.0) + max((list[i]-1) / 2, 0.0));
+                temp.push_back(max((list[i-1] - 1) / 2, 0.0) + max((list[i] - 1) / 2, 0.0));
             
             temp.push_back(temp[0]);
             list = temp;
