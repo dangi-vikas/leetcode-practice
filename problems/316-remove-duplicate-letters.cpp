729bad41d5cf1840f94f1cc5ae9677952a27efae
Vikas Dangi <mvikasdangi@gmail.com>
2023-09-26 21:54:22 +0530

    Remove Duplicate Letters

--- problems/316-remove-duplicate-letters.cpp
+++ problems/316-remove-duplicate-letters.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         string ans;
-        vector<int> count(26,0), visited(26,0);
+        vector<int> count(26, 0), visited(26, 0);
         
-        for(int i=0; i<s.size(); i++) count[s[i]-'a']++;
+        for(int i=0; i<s.size(); i++) count[s[i] - 'a']++;
         
         for(int i=0; i<s.size(); i++){
-            count[s[i]-'a']--;
+            count[s[i] - 'a']--;
             
-            if(visited[s[i]-'a']) continue;
+            if(visited[s[i] - 'a']) continue;
             visited[s[i]-'a'] = 1;
             
-            while(!ans.empty() && ans.back()>s[i] && count[ans.back()-'a']){
-                visited[ans.back()-'a'] = 0;
+            while(!ans.empty() && ans.back() > s[i] && count[ans.back() - 'a']){
+                visited[ans.back() - 'a'] = 0;
                 ans.pop_back();
             }
             
