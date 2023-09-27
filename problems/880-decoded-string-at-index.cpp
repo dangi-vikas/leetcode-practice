7514e9d4896b9e6415b49e36e33a7ef3ddc370aa
dangi-vikas <mvikasdangi@gmail.com>
2023-09-27 22:09:08 +0530

    Decoded String at Index

--- problems/880-decoded-string-at-index.cpp
+++ problems/880-decoded-string-at-index.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     string decodeAtIndex(string s, int k) {
-        long long size = 0;
+        long long count = 0;
         int n = s.length();
         string ans;
         
         for(char ch : s) {
-            if(isdigit(ch)) size *= (ch - '0');
-            else size++;
+            if(isdigit(ch)) count *= (ch - '0');
+            else count++;
         }
         
         for(int i=n-1; i>=0; i--) {
-            k %= size;
+            k %= count;
             
             if(k == 0 && isalpha(s[i])) 
                 return (ans = s[i]);
 
-            if(isdigit(s[i])) size /= (s[i] - '0');
-            else size--;
+            if(isdigit(s[i])) count /= (s[i] - '0');
+            else count--;
         }
         
         return NULL;
