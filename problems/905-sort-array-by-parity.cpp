8c3e6b889bde24a2ef60eb14d8e2aad7c9ce8647
dangi-vikas <mvikasdangi@gmail.com>
2023-09-28 23:08:52 +0530

    Sort Array by Parity

--- problems/905-sort-array-by-parity.cpp
+++ problems/905-sort-array-by-parity.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int i=0, e=nums.size()-1;
+        int i = 0, e = nums.size() - 1;
         
         while(i < e) {
-            if(nums[i]%2==1 && nums[e]%2==0) {
-                swap(nums[i],nums[e]);
+            if(nums[i] % 2 == 1 && nums[e] % 2 == 0) {
+                swap(nums[i], nums[e]);
                 i++;
                 e--;
             }
             
-            else if(nums[i]%2==0 && nums[e]%2==0)
+            else if(nums[i] % 2 == 0 && nums[e] % 2 == 0)
                 i++;
             
-            else if(nums[i]%2==1 && nums[e]%2==1)
+            else if(nums[i] % 2 == 1 && nums[e] % 2 == 1)
                 e--;
 
             else {
