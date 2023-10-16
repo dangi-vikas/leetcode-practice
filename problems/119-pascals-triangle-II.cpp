516a53db96312fef8e6a5ebdf9e0e13a89c853ca
dangi-vikas <mvikasdangi@gmail.com>
2023-10-16 21:03:37 +0530

    Pascal's Triangle - II

--- problems/119-pascals-triangle-II.cpp
+++ problems/119-pascals-triangle-II.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> ans;
+        long long c = 1;
         
         //as index start from 0
         rowIndex++;
         
         //traversing the column and pushing the required value
-        long long c = 1;
-        for(int i=1; i<=rowIndex; i++){
+        for(int i=1; i<=rowIndex; i++) {
             ans.push_back(c);
-            c = c*(rowIndex-i)/i;
+            c = c * (rowIndex-i) / i;
         }
         
         return ans;
