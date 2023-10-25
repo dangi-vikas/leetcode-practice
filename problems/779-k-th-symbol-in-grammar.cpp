3327424b1aba531391797e133b57ebf93eb07f5e
dangi-vikas <mvikasdangi@gmail.com>
2023-10-25 20:12:18 +0530

    K-th Symbol in Grammar

--- problems/779-k-th-symbol-in-grammar.cpp
+++ problems/779-k-th-symbol-in-grammar.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if(n==1 && k==1) return 0;
+        if(n == 1 && k == 1) return 0;
         
         long mid = pow(2, n-1) / 2;
         
