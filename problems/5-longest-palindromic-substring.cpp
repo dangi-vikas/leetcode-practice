d38767791fde14d9f306a71c3ef9e17078c45609
dangi-vikas <mvikasdangi@gmail.com>
2023-10-27 20:57:34 +0530

    Longest Palindromic Substring

--- problems/5-longest-palindromic-substring.cpp
+++ problems/5-longest-palindromic-substring.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int max_len = 0, len = 0, len1 = 0, len2 = 0;
+        int maxLen = 0, len = 0, len1 = 0, len2 = 0;
         int left = 0, right = 0;
 
-        for(int i = 0; i<s.length(); i++)
-        {
-            len1 = palindrome(s, i, i);
-            len2 = palindrome(s, i, i+1);
+        for(int i=0; i<s.length(); i++) {
+            len1 = palindromeLength(s, i, i);
+            len2 = palindromeLength(s, i, i+1);
+
             len = max(len1, len2);
-            if (len > max_len)
-            {
-                max_len = len;
-                left = i - (len-1)/2;
-                right = i + len/2;
+
+            if(len > maxLen) {
+                maxLen = len;
+                left = i - (len-1) / 2;
+                right = i + len / 2;
             }
         }
-
-        return s.substr(left, max_len);
+        return s.substr(left, maxLen);
     }
     
-    int palindrome (string s, int left, int right){
-        while (left>=0 && right < s.length() && s[left] == s[right])
-        {
+    int palindromeLength(string s, int left, int right) {
+        while(left >= 0 && right < s.length() && s[left] == s[right]) {
                 left--;
                 right++;            
         }
+
         return right - left - 1;
     }
 };
