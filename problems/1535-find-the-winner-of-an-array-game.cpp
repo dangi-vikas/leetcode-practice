b1f18951ef38f0ab030eea03f66cef8d0c8ca064
dangi-vikas <mvikasdangi@gmail.com>
2023-11-05 19:17:53 +0530

    Find the Winner of an Array Game

---
+++ problems/1535-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int winner = arr[0], wins = 0;
+        
+        for(int i=1; i<arr.size(); i++) {
+            if(winner > arr[i]) wins++;
+
+            else {
+                wins = 1;
+                winner = arr[i];
+            }
+            
+            if (wins == k) break;
+        }
+        
+        return winner;
+    }
+};
