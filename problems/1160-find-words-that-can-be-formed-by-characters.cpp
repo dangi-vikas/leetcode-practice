4e7e5989d695b10c47e99d061d7a8a0d460f1ee6
Vikas Dangi <mvikasdangi@gmail.com>
2023-12-02 18:18:08 +0530

    Find Words That Can Be Formed by Characters

--- problems/1160-find-words-that-can-be-formed-by-characters.cpp
+++ problems/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -4,20 +4,20 @@ public:
         vector<int> count(26);
         int ans = 0;
         
-        for (auto ch : chars) count[ch - 'a']++;
+        for(auto ch : chars) count[ch - 'a']++;
         
-        for (auto &word : words) {
+        for(auto &word : words) {
             vector<int> tempCount(26);
             bool match = true;
             
-            for (auto ch : word) {
-                if (++tempCount[ch - 'a'] > count[ch - 'a']) {
+            for(auto ch : word) {
+                if(tempCount[ch - 'a']++ > count[ch - 'a']) {
                     match = false;
                     break;
                 }
             }
             
-            if (match) ans += word.size();
+            if(match) ans += word.size();
         }
         
         return ans;
