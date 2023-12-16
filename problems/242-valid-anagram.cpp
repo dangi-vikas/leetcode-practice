8699a387d335175a2936f21b5de460c93a99633b
Vikas Dangi <mvikasdangi@gmail.com>
2023-12-16 19:33:24 +0530

    Valid Anagram

--- problems/242-valid-anagram.cpp
+++ problems/242-valid-anagram.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        
-        unordered_map<char, int> count;
-        
-        for(int i=0; i<s.size(); i++){
-            count[s[i]]++;
-            count[t[i]]--;
+        if(s.length() != t.length()) return false;
+    
+        vector<int> count(26, 0);
+
+        for(int i=0; i<s.length(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
-        
+
         /*if the count of a character is equal it second
           in count map will be zero*/
-        for(auto i=count.begin(); i!=count.end(); ++i)
-            if(i->second != 0) return false;
-        
-        return true;   
+        for(int i=0; i<26; i++) 
+            if(count[i] != 0) return false;
+
+        return true; 
     }
 };
