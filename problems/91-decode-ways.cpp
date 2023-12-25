0147e3dcbc70ede63e6660bf37bf70cee95ccf51
Vikas Dangi <mvikasdangi@gmail.com>
2023-12-25 22:07:05 +0530

    Decode Ways

--- problems/91-decode-ways.cpp
+++ problems/91-decode-ways.cpp
@@ -10,19 +10,19 @@ public:
         //count1= previous digit  count2= second previous digit
         int count1 = 1, count2 =1; 
 
-        for(int i=1; i<s.size(); i++){
+        for(int i=1; i<s.size(); i++) {
 
             //getting the integer of one digit or two digits
-            int d1 = s[i]-'0';
-            int d2 = 10 * (s[i-1]-'0') + d1;
+            int d1 = s[i] - '0';
+            int d2 = 10 * (s[i-1] - '0') + d1;
             
-            int count=0; 
+            int count = 0; 
             
             //getting the number of ways
-            if(d1>0) count += count2;
-            if(d2>=10 && d2<=26) count += count1;
+            if(d1 > 0) count += count2;
+            if(d2 >= 10 && d2 <= 26) count += count1;
             
-            count1= count2;
+            count1 = count2;
             count2 = count;
         }
         
