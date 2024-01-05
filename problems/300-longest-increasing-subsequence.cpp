6007ea125e86cd579161383dae7e6c138a679e89
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-05 22:56:52 +0530

    Longest Increasing Subsequence

--- problems/300-longest-increasing-subsequence.cpp
+++ problems/300-longest-increasing-subsequence.cpp
@@ -6,14 +6,14 @@ public:
 
         lis[0] = 1; 
         
-        for (int i = 1; i < n; i++ ) 
-        { 
+        for(int i=1; i<n; i++) { 
             lis[i] = 1; 
-            for (int j = 0; j < i; j++ ) 
-                if ( nums[i] > nums[j] && lis[i] < lis[j] + 1) 
+            
+            for(int j=0; j<i; j++) 
+                if(nums[i] > nums[j] && lis[i] < lis[j] + 1) 
                     lis[i] = lis[j] + 1; 
         } 
 
-        return *max_element(lis.begin(), lis.begin()+n);
+        return *max_element(lis.begin(), lis.begin() + n);
     }
 };
