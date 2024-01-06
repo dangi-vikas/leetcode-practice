0c3bacc639cd43169d051e3bf802db2adf4576b0
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-06 19:01:17 +0530

    Maximum Profit in Job Scheduling

--- problems/1235-maximum-profit-in-job-scheduling.cpp
+++ problems/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,8 +1,7 @@
 class Solution {
     static bool compare(pair<pair<int, int>, int>& a, pair<pair<int, int>, int>& b) {
         return a.first.second < b.first.second;
-    }
-        
+    }        
     
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
@@ -13,14 +12,13 @@ public:
         for(int i=0; i<n; i++) 
             jobs.push_back({{startTime[i], endTime[i]}, profit[i]}); 
         
-        
         sort(jobs.begin(), jobs.end(), compare);
         
-        for (pair<pair<int, int>, int>& p : jobs) {
+        for(pair<pair<int, int>, int>& p : jobs) {
             auto it = dp.upper_bound(p.first.first);
             it--;
             
-            if (dp.count(p.first.second) == 0) dp[p.first.second] = dp.rbegin()->second;
+            ifo(dp.count(p.first.second) == 0) dp[p.first.second] = dp.rbegin()->second;
             dp[p.first.second] = max(p.second + it->second, dp[p.first.second]);
         }
         
