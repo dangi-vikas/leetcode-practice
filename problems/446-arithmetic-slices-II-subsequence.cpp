3142ab8194b5e9f99b2d1755317df336fa2e784e
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-07 18:01:07 +0530

    Arithmetic Slices II - Subsequence

--- problems/446-arithmetic-slices-II-subsequence.cpp
+++ problems/446-arithmetic-slices-II-subsequence.cpp
@@ -1,23 +1,26 @@
 class Solution {
+    typedef long long ll;
+
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        long long ans = 0;
+        ll ans = 0;
         int n = nums.size();
-		vector<unordered_map<long, long>> dp(n);
+	vector<unordered_map<long, long>> dp(n);
         
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<i; j++) {
-				long long diff = (long long) nums[i] - (long long) nums[j];
+	for(int i=0; i<n; i++) {
+	    for(int j=0; j<i; j++) {
+		ll diff = (ll) nums[i] - (ll) nums[j];
 
-				dp[i][diff]++;
+		dp[i][diff]++;
                 
-				if(dp[j].find(diff) != dp[j].end()) {
-					dp[i][diff] += dp[j][diff];
-					ans += dp[j][diff];
-				}
-			}
+		if(dp[j].find(diff) != dp[j].end()) {
+		    dp[i][diff] += dp[j][diff];
+		    ans += dp[j][diff];
 		}
+	    }
+	}
         
-		return ans;
+	return ans;
     }
 };
+	
