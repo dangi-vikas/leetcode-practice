17ca2c37fb92637930f47d1c0d6f03da71b7ec5b
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-16 22:24:36 +0530

    Insert Delete GetRandom O(1)

--- problems/380-insert-delete-getrandom-o(1).cpp
+++ problems/380-insert-delete-getrandom-o(1).cpp
@@ -24,6 +24,7 @@ public:
             return false;
         
         int index = setIndexes[val];
+        
         set[index] = set[size-1];
         set.pop_back(); 
         setIndexes[set[index]] = index;
