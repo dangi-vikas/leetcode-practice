1e7433402c53942fe21812619d602017d3394fed
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-21 20:02:21 +0530

    House Robber

--- problems/198-house-robber.cpp
+++ problems/198-house-robber.cpp
@@ -3,8 +3,8 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        if(n==0) return 0;
-        if(n==1) return nums[0];
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
 
         int next = nums[n-1], nextNext = max(next, nums[n-2]);
 
@@ -18,12 +18,12 @@ public:
     }
 
     int rob(vector<int>& nums, int index, vector<int>& dp) {
-        if(index > nums.size()-1) return 0;
+        if(index > nums.size() - 1) return 0;
 
         if(dp[index] != -1) return dp[index];
 
-        int choose = nums[index] + rob(nums, index+2, dp);
-        int notChoose = rob(nums, index+1, dp);
+        int choose = nums[index] + rob(nums, index + 2, dp);
+        int notChoose = rob(nums, index + 1, dp);
 
         return dp[index] = max(choose, notChoose);
     }
