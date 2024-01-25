79e5ce40ac2ffbea3c09ee7b263049c24113d609
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-25 22:13:34 +0530

    Longest Common Subsequence

--- problems/1143-longest-common-subsequence.cpp
+++ problems/1143-longest-common-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length(), m = text2.length();
-        vector<int> prev(m+1, 0);
+        vector<int> prev(m+1);
 
         for(int i=1; i<=n; i++) {
             vector<int> curr(m+1, 0);
@@ -10,7 +10,7 @@ public:
             for(int j=1; j<=m; j++) {
                 if(text1[i-1] == text2[j-1]) 
                     curr[j] = 1 + prev[j-1];
-
+                    
                 else curr[j] = max(prev[j], curr[j-1]);
             }
 
