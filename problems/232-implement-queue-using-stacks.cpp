a52bd7ce6070149042850d5b45346ff0644383d6
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-29 20:49:56 +0530

    Implement Queue using Stacks

--- problems/232-implement-queue-using-stacks.cpp
+++ problems/232-implement-queue-using-stacks.cpp
@@ -1,37 +1,51 @@
 class MyQueue {
-public:
     stack<int> s1;
     stack<int> s2;
     
+public:
+    /** Initialize your data structure here. */
     MyQueue() {
         
     }
     
+    /** Push element x to the back of queue. */
     void push(int x) {
-        while(!s1.empty()){
-        s2.push(s1.top());
-        s1.pop();
-        }
-        s2.push(x);
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
+       s1.push(x); 
     }
     
+    /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        int temp= s1.top();
-        s1.pop();
-        return temp;
+        if(s1.empty() && s2.empty())
+            return -1;
+        
+        if(s2.empty()) {
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int k = s2.top();
+        s2.pop();
+
+        return k;
     }
     
+    /** Get the front element. */
     int peek() {
-        return s1.top();
+        if(s2.empty()) {
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        return s2.top();
     }
     
+    /** Returns whether the queue is empty. */
     bool empty() {
-        if(s1.empty()) return true;
-        return false;
+        return s1.empty() && s2.empty();
     }
 };
 
@@ -43,3 +57,16 @@ public:
  * int param_3 = obj->peek();
  * bool param_4 = obj->empty();
  */
+        
+
+    
+        
+
+
+        
+
+    
+        
+
+
+
