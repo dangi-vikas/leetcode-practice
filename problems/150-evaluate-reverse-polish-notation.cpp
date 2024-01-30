50d3695b08dbe6c6b1de3d728e7490f760c6fb22
Vikas Dangi <mvikasdangi@gmail.com>
2024-01-30 22:21:03 +0530

    Evaluate Reverse Polish Notation

--- problems/150-evaluate-reverse-polish-notation.cpp
+++ problems/150-evaluate-reverse-polish-notation.cpp
@@ -3,20 +3,23 @@ public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
         
-        for(auto t : tokens){
+        for(auto t : tokens) {
 
             //if the character is a operator
-            if(t == "+" || t == "-" || t == "*" || t == "/"){
-                int op1 = st.top(); st.pop();
-                int op2 = st.top(); st.pop();
+            if(t == "+" || t == "-" || t == "*" || t == "/") {
+                int op1 = st.top(); 
+                st.pop();
+                
+                int op2 = st.top(); 
+                st.pop();
                 
                 //performing the operation
-                if(t=="+") st.push(op1 + op2);
-                if(t=="-") st.push(op2 - op1);
-                if(t=="*") st.push(op1 * op2);
-                if(t=="/") st.push(op2 / op1);
+                if(t == "+") st.push(op1 + op2);
+                if(t == "-") st.push(op2 - op1);
+                if(t == "*") st.push(op1 * op2);
+                if(t == "/") st.push(op2 / op1);
             }
-            
+
             else st.push(stoi(t));
         }
         
