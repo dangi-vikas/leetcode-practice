2f36a8ac29d699075331e2cf96bd60019f4bab2f
Vikas Dangi <mvikasdangi@gmail.com>
2024-02-05 20:51:19 +0530

    First Unique Character in a String

--- problems/387-first-unique-character-in-a-string.cpp
+++ problems/387-first-unique-character-in-a-string.cpp
@@ -3,11 +3,11 @@ public:
     int firstUniqChar(string s) {
         vector<int> ans(26, 0);
         
-        for (auto x: s)
+        for(auto x : s)
             ans[x - 'a']++;
         
-        for (int i = 0; i < s.size(); i++) 
-            if (ans[s[i] - 'a'] == 1) return i;
+        for(int i=0; i<s.size(); i++) 
+            if(ans[s[i] - 'a'] == 1) return i;
         
         return -1;
     }
