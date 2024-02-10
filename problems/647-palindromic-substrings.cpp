9c0e91a3d7052adb8b9643514e57f55e1bc7b3b8
Vikas Dangi <mvikasdangi@gmail.com>
2024-02-10 22:34:37 +0530

    Palindromic Substrings

--- problems/647-palindromic-substrings.cpp
+++ problems/647-palindromic-substrings.cpp
@@ -3,8 +3,7 @@ public:
     int countSubstrings(string s) {
         if(s.length() == 0) return 0;
         
-        int n = s.length(); 
-        int ans = 0; 
+        int n = s.length(), ans = 0; 
         
         for(int i=0; i<n; i++) {
             ans += isPalindrome(i, i, s);
@@ -17,7 +16,7 @@ public:
     int isPalindrome(int start, int end, string& s) {
         int count = 0;
             
-        while(start>=0 && end<s.length() && s[start--] == s[end++])
+        while(start >= 0 && end < s.length() && s[start--] == s[end++])
             count++;
         
         return count;
