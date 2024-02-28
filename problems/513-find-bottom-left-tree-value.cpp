f2d5b364893f4a0a1bdffc6ba8e1e033cae27619
Vikas Dangi <mvikasdangi@gmail.com>
2024-02-28 23:11:14 +0530

    Find Bottom Left Tree Value

--- problems/513-find-bottom-left-tree-value.cpp
+++ problems/513-find-bottom-left-tree-value.cpp
@@ -19,14 +19,14 @@ public:
         queue<TreeNode*> q;
         q.push(root);
         
-        while(!q.empty()){
+        while(!q.empty()) {
             int n = q.size();
             
             for(int i=0; i<n; i++){
                 TreeNode* node = q.front();
                 q.pop();
                 
-                if(i==0) ans = node->val;
+                if(i == 0) ans = node->val;
                 
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
