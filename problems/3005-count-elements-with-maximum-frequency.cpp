5df025c781b4a6a1e429d5b4e78ab63a1f9f798d
Vikas Dangi <mvikasdangi@gmail.com>
2024-03-08 21:15:12 +0530

    count-elements-with-maximum-frequency.cpp

---
+++ problems/3005-count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int, int> frequencies;
+        int maxFrequency = 0, totalFrequencies = 0;
+
+        for(int num : nums) {
+            frequencies[num]++;
+            int frequency = frequencies[num];
+
+            if(frequency > maxFrequency) {
+                maxFrequency = frequency;
+                totalFrequencies = frequency;
+            } 
+            
+            else if(frequency == maxFrequency) 
+                totalFrequencies += frequency;
+        }
+        
+        return totalFrequencies;
+    }
+};
