660e0f159aecff97e4719c06e987ed7647aa09cd
Vikas Dangi <mvikasdangi@gmail.com>
2024-03-13 22:19:21 +0530

    Find the Pivot Integer

---
+++ problems/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        double x = sqrt(n * (n + 1) / 2.0);
+        
+        if (fmod(x, 1.0) != 0) 
+            return -1;
+       
+        return static_cast<int>(x);
+    }
+};
