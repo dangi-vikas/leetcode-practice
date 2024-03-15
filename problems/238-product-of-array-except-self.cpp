1a80b9f58957b04eec1b99978351c111d1287c94
Vikas Dangi <mvikasdangi@gmail.com>
2024-03-15 21:10:39 +0530

    Product of Array Except Self

--- problems/238-product-of-array-except-self.cpp
+++ problems/238-product-of-array-except-self.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n,1);
-        int productLeft=1, productRight=1;
+        vector<int> ans(n, 1);
+        int productLeft = 1, productRight = 1;
        
-        for(int i=1; i<n; i++)
-        {
+        for(int i=1; i<n; i++) {
             productLeft *= nums[i-1];
             productRight *= nums[n-i];
  
