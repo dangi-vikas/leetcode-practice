2072bcd9f30ef1612c1a727750c6e7c2912f63dd
Vikas Dangi <mvikasdangi@gmail.com>
2024-03-17 19:21:14 +0530

    Insert Interval

--- problems/57-insert-interval.cpp
+++ problems/57-insert-interval.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int>> ans;
         
         //if intervals is empty
-        if(intervals.size() == 0){
+        if(intervals.size() == 0) {
             ans.push_back(newInterval);  
             return ans;
         } 
@@ -12,15 +12,15 @@ public:
         bool isMerged = false;
         
         //traversing intervals
-        for(int i=0; i<intervals.size(); i++){
+        for(int i=0; i<intervals.size(); i++) {
 
             //if the newInterval is subset of one of the intervals of (intervals vector)
-            if(intervals[i][1] >= newInterval[0]){
+            if(intervals[i][1] >= newInterval[0]) {
                 int j = i;
                 int minI = newInterval[0];
                 int maxI = newInterval[1];
                 
-                while(j < intervals.size() && intervals[j][0] <= newInterval[1]){
+                while(j < intervals.size() && intervals[j][0] <= newInterval[1]) {
                     minI = min(intervals[j][0], minI);
                     maxI = max(intervals[j][1], maxI);
                     j++;
@@ -30,22 +30,23 @@ public:
                 ans.push_back({minI, maxI});
                 isMerged = true;
                 
-                while(j < intervals.size()){
+                while(j < intervals.size()) {
                     ans.push_back(intervals[j]);
                     j++;
                 }
 
                 break;
             }
+                
             else ans.push_back(intervals[i]);
         }
 
         //if newInterval is greater than every interval
-        if(isMerged == false){
+        if(isMerged == false) {
             ans.push_back(newInterval);
             return ans;
         }
-
+        
         return ans;
     }
 };
