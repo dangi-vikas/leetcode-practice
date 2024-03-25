3763c9d4d260363f378faede2f761d23f857b153
Vikas Dangi <mvikasdangi@gmail.com>
2024-03-25 23:06:14 +0530

    Find All Duplicates in an Array

--- problems/442-find-all-duplicates-in-an-array.cpp
+++ problems/442-find-all-duplicates-in-an-array.cpp
@@ -3,10 +3,11 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> ans;
         
-        for(int n : nums){
-            n = abs(n);
-            if(nums[n-1] > 0) nums[n-1] *= -1;
-            else ans.push_back(n);
+        for(int num : nums) {
+            num = abs(num);
+            
+            if(nums[num-1] > 0) nums[num-1] *= -1;
+            else ans.push_back(num);
         }
         
         return ans;
