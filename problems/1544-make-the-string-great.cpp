d32318f991161c7e71eb942c36909272af3c571c
Vikas Dangi <mvikasdangi@gmail.com>
2024-04-05 14:36:08 +0530

    Make the String Great

--- problems/1544-make-the-string-great.cpp
+++ problems/1544-make-the-string-great.cpp
@@ -3,7 +3,7 @@ public:
     string makeGood(string s) {
         int i = 0;
         
-        while (i < s.length()) {
+        while(i < s.length()) {
             if(abs(s[i+1] - s[i]) == 32) {
                 s.erase(i, 2);
                 i = 0;
