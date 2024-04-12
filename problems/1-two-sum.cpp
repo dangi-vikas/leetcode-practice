d620518092fb44dfd83838c46289c65ab1912710
Vikas Dangi <mvikasdangi@gmail.com>
2024-04-12 23:57:57 +0530

    Two Sum

--- problems/1-two-sum.cpp
+++ problems/1-two-sum.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0; i<len-1; i++) {
             for(int j=i+1; j<len; j++) {
                 if(nums[i] + nums[j] == target){                //when the two numbers' sum is equal to target
-                    v.push_back(i);
+                    v.push_back(i); 
                     v.push_back(j);                             //pushing them in the vector
                 }
             } 
