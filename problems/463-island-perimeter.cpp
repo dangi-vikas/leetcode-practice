cdcc14399b3ba6296e0b340842b4226e50000422
Vikas Dangi <mvikasdangi@gmail.com>
2024-04-18 23:33:59 +0530

    Island Perimeter

--- problems/463-island-perimeter.cpp
+++ problems/463-island-perimeter.cpp
@@ -4,16 +4,17 @@ public:
         int m = grid.size(), n = grid[0].size();
         int count = 0;
         
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++){
-                if(grid[i][j]==1){
-                    count+=4;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == 1) {
+                    count += 4;
                     
                     if(i>0 && grid[i-1][j] == 1) count -= 2;
                     if(j>0 && grid[i][j-1] == 1) count -= 2;
                 }
             }
-        
+        }
+
         return count;
     }
 };
