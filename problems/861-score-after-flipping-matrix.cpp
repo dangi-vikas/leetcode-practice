93dd9463b13b285a8fbd65b6fe1f47072f498395
Vikas Dangi <mvikasdangi@gmail.com>
2024-05-13 23:12:13 +0530

    Score After Flipping Matrix

--- problems/861-score-after-flipping-matrix.cpp
+++ problems/861-score-after-flipping-matrix.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int matrixScore(vector<vector<int>>& grid) {
-        for(int i=0; i<grid.size(); i++)
-            if(grid[i][0] == 0)
+        for(int i=0; i<grid.size(); i++) {
+            if(grid[i][0] == 0) {
                 for(auto &j : grid[i]) j = !j;
+            }
+        }
 
             int m = grid.size();
             int n = grid[0].size();
@@ -13,8 +15,9 @@ public:
         
             tt >>= 1;
 
-            for(int i=1; i<n; i++){
+            for(int i=1; i<n; i++) {
                 int t = 0;
+                
                 for(int j=0; j<m; j++)
                     if(grid[j][i] == 1) t++;
 
