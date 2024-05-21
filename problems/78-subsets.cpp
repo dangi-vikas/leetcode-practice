bb97c377816ee0050bca2a0a81be3e93fa7d08ef
Vikas Dangi <mvikasdangi@gmail.com>
2024-05-21 22:31:50 +0530

    Subsets

--- problems/78-subsets.cpp
+++ problems/78-subsets.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-		vector<int> container;
-
-		subsetsRec(ans, nums, container, 0);
-
-		return ans;
+	vector<int> container;
+	subsetsRec(ans, nums, container, 0);
+	return ans;
     }
     
-	void subsetsRec(vector<vector<int>>& ans, vector<int>& nums,
-                 vector<int>& container, int index) {
-        
-        //pushing the subset in the ans array
-		ans.push_back(container);
-        
-		for (int i = index; i < nums.size(); ++i) {
-            
+    void subsetsRec(vector<vector<int>>& ans, vector<int>& nums,
+        vector<int>& container, int index) {
+	
+	//pushing the subset in the ans array
+	ans.push_back(container);
+
+	for(int i = index; i < nums.size(); ++i) {
+    
             //pushing the numbers in the temporary container
-			container.push_back(nums[i]);
+	    container.push_back(nums[i]);
 
-            //recursively traversing the nums for the next element
-			subsetsRec(ans, nums, container, i + 1);
+    	    //recursively traversing the nums for the next element
+	    subsetsRec(ans, nums, container, i + 1);
 
-            //backtracking step
-			container.pop_back();
-        }
+    	   //backtracking step
+           container.pop_back();
+	}
     }
 };
