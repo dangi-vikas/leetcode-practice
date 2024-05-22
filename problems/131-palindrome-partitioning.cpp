0b93130b36e6b1af5b97565e1a70fda9cc20b8dd
Vikas Dangi <mvikasdangi@gmail.com>
2024-05-22 21:26:39 +0530

    Palindrome Partitioning

--- problems/131-palindrome-partitioning.cpp
+++ problems/131-palindrome-partitioning.cpp
@@ -5,20 +5,21 @@ public:
         vector<string> container;
  
         partitionRec(s, ans, container, 0);
+        
         return ans;
     }
     
     void partitionRec(string& s, vector<vector<string>>& ans, vector<string>& container, int index){
         
         //base condition
-        if(index == s.size()){
+        if(index == s.size()) {
             ans.push_back(container);
             return;
         }
         
         //iterating through string
-        for(int i=index; i<s.size(); i++){
-            if(isPalindrome(s, index, i)){
+        for(int i=index; i<s.size(); i++) {
+            if(isPalindrome(s, index, i)) {
 
                 //inserting a palindromic substring
                 container.push_back(s.substr(index, i-index+1));
@@ -33,10 +34,12 @@ public:
     }
     
     //function to check if the string is palindrome
-    bool isPalindrome(string& str, int l, int r){
-        while(l<=r){
-            if(str[l++] != str[r--]) return false;
+    bool isPalindrome(string& str, int l, int r) {
+        while(l<=r) {
+            if(str[l++] != str[r--]) 
+                return false;
         }
+        
         return true;
     }
 };
