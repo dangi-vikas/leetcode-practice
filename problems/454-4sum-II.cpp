515a1338df1ef360ff33fe66a372cccaae706e75
Vikas Dangi <mvikasdangi@gmail.com>
2024-06-04 22:19:57 +0530

    Update 454-4sum-II.cpp
    4Sum II

--- problems/454-4sum-II.cpp
+++ problems/454-4sum-II.cpp
@@ -4,19 +4,21 @@ public:
         unordered_map<int, int> sums;
         int count = 0;
         
-        for(int num1 : nums1)
-            for(int num2 : nums2){
+        for(int num1 : nums1) {
+            for(int num2 : nums2) {
                 int sum = num1 + num2;
                 sums[-sum]++;
             }
+        }
         
-        for(int num3 : nums3)
-            for(int num4 : nums4){
+        for(int num3 : nums3) {
+            for(int num4 : nums4) {
                 int sum = num3 + num4;
                 
                 if(sums.find(sum) != sums.end())
                     count += sums[sum];
             }
+        }
         
         return count;
     }
