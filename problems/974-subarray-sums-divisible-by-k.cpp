fdd3c9daee92e6863c7b4aabaae6c8193a0328e5
Vikas Dangi <mvikasdangi@gmail.com>
2024-06-09 23:36:03 +0530

    Subarray Sums Divisible by K

--- problems/974-subarray-sums-divisible-by-k.cpp
+++ problems/974-subarray-sums-divisible-by-k.cpp
@@ -3,17 +3,17 @@ public:
     int subarraysDivByK(vector<int>& nums, int k) {
         int n = nums.size();
         int ans = 0, sum = 0;
-	    vector<int> pref(k, 0);
+	vector<int> pref(k, 0);
 
-	    pref[0]++;
+	pref[0]++;
 
-	    for(int i=0; i<n; i++) {
-	        sum += nums[i];
-	        int rem = (sum % k + k) % k;
-	        ans += pref[rem];
-	        pref[rem]++;
-	    }
+	for(int i=0; i<n; i++) {
+	    sum += nums[i];
+	    int rem = (sum % k + k) % k;
+	    ans += pref[rem];
+	    pref[rem]++;
+	}
 
-	    return ans;
+	return ans;
     }
 };
