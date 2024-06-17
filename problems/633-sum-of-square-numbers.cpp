83352319f89c3ac0d4e687552d9618aa3b3a3c50
Vikas Dangi <mvikasdangi@gmail.com>
2024-06-17 21:44:42 +0530

    Sum of Square Numbers

--- problems/633-sum-of-square-numbers.cpp
+++ problems/633-sum-of-square-numbers.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        long low=0, high=sqrt(c);
+        long low = 0, high = sqrt(c);
         
         while(low <= high) {
-            long total = high*high + low*low;
+            long total = (high * high) + (low * low);
             
             if(total == c) return true;
             else if(total < c) low++;
