ad52475c2b8965aa8ad182ab58439c2c000a9627
Vikas Dangi <mvikasdangi@gmail.com>
2024-06-18 20:27:50 +0530

    Most Profit Assigning Work

--- problems/826-most-profit-assigning-work.cpp
+++ problems/826-most-profit-assigning-work.cpp
@@ -3,11 +3,11 @@ public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         map<int, int> maxes;
         
-        for(int i = 0;i<difficulty.size();i++) {
+        for(int i=0; i<difficulty.size(); i++) {
             if(maxes.find(difficulty[i]) == maxes.end()) 
                 maxes[difficulty[i]] = profit[i];
     
-            else maxes[difficulty[i]] = max(maxes[difficulty[i]],profit[i]);
+            else maxes[difficulty[i]] = max(maxes[difficulty[i]], profit[i]);
         }
         
         vector<int> uniqueDifficulties;
@@ -15,19 +15,19 @@ public:
         for(auto& i : maxes)
             uniqueDifficulties.push_back(i.first);
         
-        vector<int> ordering(uniqueDifficulties.size()+1, INT_MIN); 
+        vector<int> ordering(uniqueDifficulties.size() + 1, INT_MIN); 
         
-        for(int i = 1; i<uniqueDifficulties.size() + 1; i++)
-            ordering[i] = max(ordering[i-1],maxes[uniqueDifficulties[i-1]]);
+        for(int i=1; i<uniqueDifficulties.size()+1; i++)
+            ordering[i] = max(ordering[i-1], maxes[uniqueDifficulties[i-1]]);
         
-        int tot = 0;
+        int total = 0;
         
-        for(int i = 0; i<worker.size(); i++) {    
-            int index = upper_bound(uniqueDifficulties.begin(),uniqueDifficulties.end(),worker[i])-uniqueDifficulties.begin()-1;
+        for(int i=0; i<worker.size(); i++) {    
+            int index = upper_bound(uniqueDifficulties.begin(), uniqueDifficulties.end(), worker[i]) - uniqueDifficulties.begin() - 1;
             if(index == -1) continue; 
-            tot += ordering[index+1];
+            total += ordering[index+1];
         }
         
-        return tot;
+        return total;
     } 
 };
