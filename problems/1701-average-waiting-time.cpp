88a07552dd5f9bd025fb5f407dfbfa81361bbfc7
Vikas Dangi <mvikasdangi@gmail.com>
2024-07-09 22:58:34 +0530

    Average Waiting Time

---
+++ problems/1701-average-waiting-time.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        double totalWaitingTime = 0;
+        int currentTime = 0;
+
+        for (const auto& customer : customers) {
+            int arrival = customer[0];
+            int service = customer[1];
+            
+            if (currentTime < arrival) {
+                currentTime = arrival;
+            }
+            
+            int waitingTime = currentTime - arrival + service;
+            totalWaitingTime += waitingTime;
+            currentTime += service;
+        }
+
+        return totalWaitingTime / customers.size();
+    }
+};
