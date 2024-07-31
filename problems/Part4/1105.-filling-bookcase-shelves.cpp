0c2730b81b3a8ddf528e6caa1d1efa3c88def8b3
Vikas Dangi <mvikasdangi@gmail.com>
2024-07-31 22:37:06 +0530

    Filling Bookcase Shelves

---
+++ problems/Part4/1105.-filling-bookcase-shelves.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int n = books.size();
+        vector<int> dp(n+1, INT_MAX);
+        
+        dp[0] = 0;  
+        
+        for(int i=1; i<=n; i++) {
+            int totalWidth = 0;
+            int maxHeight = 0;
+            
+            for(int j=i; j>0; j--) {
+                totalWidth += books[j-1][0];
+                
+                if (totalWidth > shelfWidth) {
+                    break;
+                }
+                
+                maxHeight = max(maxHeight, books[j-1][1]);
+                dp[i] = min(dp[i], dp[j-1] + maxHeight);
+            }
+        }
+        
+        return dp[n];
+    }
+};
