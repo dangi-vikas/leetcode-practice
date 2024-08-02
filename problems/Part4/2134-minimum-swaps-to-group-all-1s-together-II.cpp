ca18db30a7c98d17d5e227e04dc4b58d01d74e5f
Vikas Dangi <mvikasdangi@gmail.com>
2024-08-02 23:56:40 +0530

    Minimum Swaps to Group All 1's Together II

---
+++ problems/Part4/2134-minimum-swaps-to-group-all-1s-together-II.cpp
@@ -0,0 +1,25 @@
+ class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int n = nums.size();
+        int totalOnes = 0, currentOnes = 0;
+        
+        for(int num : nums) totalOnes += num;
+        
+        if(totalOnes == 0 || totalOnes == n) return 0;
+        
+        for(int i=0; i<totalOnes; i++) {
+            currentOnes += nums[i];
+        }
+        
+        int maxOnes = currentOnes;
+        
+        for(int i=0; i<n; i++) {
+            currentOnes -= nums[i];
+            currentOnes += nums[(i + totalOnes) % n];
+            maxOnes = max(maxOnes, currentOnes);
+        }
+        
+        return totalOnes - maxOnes;
+    }
+};
