7a21231c9c5b3712fcd8a7357131097f9be72a05
Vikas Dangi <mvikasdangi@gmail.com>
2024-08-22 23:12:27 +0530

    Number Complement

--- problems/Part3/476-number-complement.cpp
+++ problems/Part3/476-number-complement.cpp
@@ -3,8 +3,9 @@ public:
     int findComplement(int num) {
         long i = 0, ans = 0;
         
-        while (num != 0){
-            if ((num&1) == 0) ans += pow(2,i);  
+        while (num != 0) {
+            if ((num & 1) == 0) ans += pow(2,i);  
+            
             i++;  
             num = num >> 1; 
         }
